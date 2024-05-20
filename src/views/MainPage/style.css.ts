a9c94d0b1212a350481321918d7d3a5eab7246b0
Jeon Eonseok <jseokeon@gmail.com>
2024-05-20 17:34:04 +0900

    design: style 예시 수정

--- src/views/MainPage/style.css.ts
+++ src/views/MainPage/style.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 import { vars } from 'styles/theme.css';
 
 export const container = style({
-  color: vars.theme.colors.color,
-  backgroundColor: vars.theme.colors.backgroundColor,
+  color: vars.theme.colors.red50,
+  backgroundColor: vars.theme.colors.yellow400,
   ...vars.theme.fonts.LABEL_2_16_SB,
 });
