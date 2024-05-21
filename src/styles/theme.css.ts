a6499c8b7dfdf75f8b2d063504164489845d0b31
Jeon Eonseok <jseokeon@gmail.com>
2024-05-22 01:28:41 +0900

    design: theme에서 fonts 제거

--- src/styles/theme.css.ts
+++ src/styles/theme.css.ts
@@ -4,22 +4,18 @@ import {
   createTheme,
   createThemeContract,
 } from '@vanilla-extract/css';
-import { fonts } from './font.css';
 
 const root = createGlobalTheme('#root', {});
 
 const theme = createThemeContract({
-  fonts: fonts,
   colors: colors,
 });
 
 export const light = createTheme(theme, {
-  fonts: fonts,
   colors: colors,
 });
 
 export const dark = createTheme(theme, {
-  fonts: fonts,
   colors: colors,
 });
 
