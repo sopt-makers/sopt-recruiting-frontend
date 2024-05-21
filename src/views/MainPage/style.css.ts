d0088de5bea26a9965d709dd2234d1ff44adbb48
Jeon Eonseok <jseokeon@gmail.com>
2024-05-22 01:35:10 +0900

    design: css 예시 수정

--- src/views/MainPage/style.css.ts
+++ src/views/MainPage/style.css.ts
@@ -1,8 +1,9 @@
 import { style } from '@vanilla-extract/css';
 import { vars } from 'styles/theme.css';
+import { fontsObject } from '@sopt-makers/fonts';
 
 export const container = style({
   color: vars.theme.colors.red50,
   backgroundColor: vars.theme.colors.yellow400,
-  ...vars.theme.fonts.LABEL_2_16_SB,
+  ...fontsObject.BODY_1_18_M,
 });
