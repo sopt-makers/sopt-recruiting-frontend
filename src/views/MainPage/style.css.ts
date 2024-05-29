75c45c1610028795b215281c2a735b90bee3800a
Jeon Eonseok <jseokeon@gmail.com>
2024-05-29 19:46:45 +0900

    chore: style 예제 수정

--- src/views/MainPage/style.css.ts
+++ src/views/MainPage/style.css.ts
@@ -3,7 +3,7 @@ import { vars } from 'styles/theme.css';
 import { fontsObject } from '@sopt-makers/fonts';
 
 export const container = style({
-  color: vars.theme.primaryFont,
+  color: vars.theme.baseFont,
   backgroundColor: vars.theme.background,
   ...fontsObject.BODY_1_18_M,
 });
