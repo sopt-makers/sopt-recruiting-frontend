feeffc7636dc68efd8ef3ac26a2165c30da1cada
lydiacho <sheecho0804@ewhain.net>
2024-06-04 17:16:30 +0900

    design: input 기본 스타일링

--- src/common/components/Input/index.tsx
+++ src/common/components/Input/index.tsx
@@ -1,5 +1,27 @@
+import { container, circle, input, label, description } from './style.css';
+
+const DescriptionButton = ({ children }: { children: string }) => {
+  return (
+    <button type="button" style={{ cursor: 'pointer' }}>
+      {children}
+    </button>
+  );
+};
+
 const Input = () => {
-  return <div>index</div>;
+  return (
+    <div className={container}>
+      <label className={label}>
+        <span>타이틀</span>
+        <i className={circle} />
+      </label>
+      <input className={input} placeholder="디폴트" />
+      <div className={description}>
+        <p>비밀번호를 잃어버리셨나요?</p>
+        <DescriptionButton>비밀번호 재설정하기</DescriptionButton>
+      </div>
+    </div>
+  );
 };
 
 export default Input;
--- src/common/components/Input/style.css.ts
+++ src/common/components/Input/style.css.ts
@@ -1,3 +1,51 @@
 import { style } from '@vanilla-extract/css';
 
-import { vars } from 'styles/theme.css';
+import { theme } from 'styles/theme.css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 8,
+
+  color: theme.color.black,
+  ...theme.font.BODY_1_18_M,
+});
+
+export const label = style({
+  display: 'flex',
+  alignItems: 'center',
+  gap: 6,
+
+  color: theme.color.baseText,
+  ...theme.font.TITLE_5_18_SB,
+});
+
+export const circle = style({
+  width: 8,
+  height: 8,
+  borderRadius: 4,
+  backgroundColor: theme.color.primary,
+});
+
+export const input = style({
+  padding: 16,
+  backgroundColor: theme.color.white,
+  borderRadius: 12,
+  border: `1px solid ${theme.color.border}`,
+
+  color: theme.color.baseText,
+  ...theme.font.BODY_2_16_R,
+
+  '::placeholder': {
+    color: theme.color.placeholder,
+    ...theme.font.BODY_2_16_R,
+  },
+});
+
+export const description = style({
+  display: 'flex',
+  gap: 10,
+
+  color: theme.color.lightestText,
+  ...theme.font.LABEL_2_16_SB,
+});
