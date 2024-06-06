d2e6a6f7dcf0bfa16abb0b5a0f292f30a04de6f5
Jeon Eonseok <jseokeon@gmail.com>
2024-06-06 11:12:26 +0900

    feat: maxCount 동적으로 받기

--- src/common/components/Textarea/TextareaHeader/index.tsx
+++ src/common/components/Textarea/TextareaHeader/index.tsx
@@ -4,14 +4,16 @@ import type { HTMLAttributes, ReactNode } from 'react';
 
 interface TextareaHeaderProps extends HTMLAttributes<HTMLHeadingElement> {
   children: ReactNode;
+  maxCount: number;
   required?: boolean;
 }
 
-const TextareaHeader = ({ children, required = false, ...headerElementProps }: TextareaHeaderProps) => {
+const TextareaHeader = ({ children, maxCount, required = false, ...headerElementProps }: TextareaHeaderProps) => {
   return (
     <h4 className={`${textareaHeaderStyle}`} {...headerElementProps}>
       <p>
-        {children}&nbsp;
+        <span>{children}</span>
+        <span> ({maxCount}자) </span>
         <span className={`${required && requireDot}`} />
       </p>
     </h4>
