bb6b1268b46104ead98dbd8a8de56a22d3440a09
Jeon Eonseok <jseokeon@gmail.com>
2024-06-09 21:48:48 +0900

    chore: component name 수정

--- src/common/components/Checkbox/Contentbox/index.tsx
+++ src/common/components/Checkbox/Contentbox/index.tsx
@@ -2,16 +2,16 @@ import { container } from './style.css';
 
 import type { HTMLAttributes, ReactNode } from 'react';
 
-interface ContentBoxProps extends HTMLAttributes<HTMLElement> {
+interface ContentboxProps extends HTMLAttributes<HTMLElement> {
   children?: ReactNode;
 }
 
-const ContentBox = ({ children, ...contentBoxElementProps }: ContentBoxProps) => {
+const Contentbox = ({ children, ...contentboxElementProps }: ContentboxProps) => {
   return (
-    <article className={container} {...contentBoxElementProps}>
+    <article className={container} {...contentboxElementProps}>
       {children}
     </article>
   );
 };
 
-export default ContentBox;
+export default Contentbox;
