f159e48b2d25c89dd431bcde4e38535f99edb7f0
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-06-10 21:00:14 +0900

    [Feat] Input 공통 컴포넌트 구현 (#26)
    * feat: index, style 파일 생성
    
    * design: input 기본 스타일링
    
    * feat: Input Props interface
    
    * feat: 합성컴포넌트 패턴 적용
    
    * move: interface 파일 분리
    
    * chore: 작업공간 MainPage -> ApplyPage
    
    * feat: 합성컴포넌트 철회
    
    * feat: 두줄짜리 input 처리 가능하도록 수정
    
    * fix: mds에 반영안된 gray20 -> 컬러코드로 추가
    
    * feat: isFixed에 따라 disabled 처리
    
    * fix: value, onChange 등을 위해 inputAttr Pick 해제
    
    * feat: descriptionButton 책임 제거, button Node 추가
    
    * install: react-hook-form 설치
    
    * feat: register 활용
    
    * feat: 에러 조건부 처리
    
    * feat: 에러 초기화 기준 onBlur -> onChange
    
    * feat: useForm반환값 props 객체로 넘기기
    
    * feat: size prop 추가
    
    * feat: maxLength 조건 별도로 분리
    
    * chore: 불필요한 Omit 제거
    
    * chore: 불필요한 Omit 제거
    
    * feat: type 속성 추가
    
    * fix: classList->stylecomposition 방식으로 변경
    
    * fix: styleVariant 적용 및 htmlAttr 사용 확대
    
    * fix: inputElementProps spread 연산자 적용
    
    * fix: 합성컴포넌트패턴 및 useContext 적용
    
    * feat: 에러 텍스트 명시 고도화
    
    * fix: clearError onFocus -> onChange로 변경
    
    * fix: 에러 체크 기준 수정사항 반영
    
    * fix: 타입에러 모두 해결
    
    * fix: 코드리뷰 반영

--- src/pages/ApplyPage.tsx
+++ src/pages/ApplyPage.tsx
@@ -1,5 +1,55 @@
+import TextBox from '@components/Input';
+import { TFormValues } from '@components/Input/types';
+import { SubmitHandler, useForm } from 'react-hook-form';
+
+const MoreButton = () => {
+  return (
+    <button type="button" style={{ cursor: 'pointer' }} onClick={() => console.log('버튼클릭')}>
+      더알아보는버튼
+    </button>
+  );
+};
+
+const CheckButton = () => {
+  return (
+    <button
+      type="button"
+      style={{ cursor: 'pointer', backgroundColor: 'green', padding: 10 }}
+      onClick={() => console.log('버튼클릭')}>
+      인증하기
+    </button>
+  );
+};
+
 const ApplyPage = () => {
-  return <div>ApplyPage</div>;
+  const { handleSubmit, ...formObject } = useForm();
+
+  const onSubmit: SubmitHandler<TFormValues> = (data) => console.log(data);
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <TextBox.Container label="이메일" required formObject={formObject}>
+        <TextBox.InputLine
+          label="이메일"
+          button={<CheckButton />}
+          placeholder="이메일을 입력하세요"
+          pattern={/^[0-9]*$/}
+          errorText="숫자만 입력해주세요"
+        />
+        <TextBox.InputLine
+          label="인증번호"
+          placeholder="인증번호를 입력하세요"
+          validate={(_, v) => v['이메일'] === '검사조건' || '틀렸어요'}
+          errorText="틀렸습니다"
+        />
+        <TextBox.Description>
+          <p>더 알아보는 텍스트</p>
+          <MoreButton />
+        </TextBox.Description>
+      </TextBox.Container>
+      <input type="submit" value="제출버튼!" style={{ backgroundColor: 'green', marginTop: 30 }} />
+    </form>
+  );
 };
 
 export default ApplyPage;
--- src/views/MainPage/style.css.ts
+++ src/views/MainPage/style.css.ts
@@ -3,7 +3,5 @@ import { style } from '@vanilla-extract/css';
 import { theme } from 'styles/theme.css';
 
 export const container = style({
-  color: theme.color.primaryDark,
-  backgroundColor: theme.color.primaryDark,
   ...theme.font.BODY_1_18_M,
 });
