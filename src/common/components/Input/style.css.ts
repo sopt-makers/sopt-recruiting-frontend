79400fa27a3857a223b767b1bd7533b643fb724f
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-06-11 03:04:32 +0900

    [Merge] feat/#15_input => develop (#33)
    * feat: index, style 파일 생성
    
    * design: input 기본 스타일링
    
    * feat: Input Props interface
    
    * feat: 합성컴포넌트 패턴 적용
    
    * move: interface 파일 분리
    
    * chore: 작업공간 MainPage -> ApplyPage
    
    * feat: 합성컴포넌트 철회
    
    * feat: 두줄짜리 input 처리 가능하도록 수정
    
    * fix: mds에 반영안된 gray20 -> 컬러코드로 추가
    
    * feat: isFixed에 따라 disabled 처리
    
    * fix: value, onChange 등을 위해 inputAttr Pick 해제
    
    * feat: descriptionButton 책임 제거, button Node 추가
    
    * install: react-hook-form 설치
    
    * feat: register 활용
    
    * feat: 에러 조건부 처리
    
    * feat: 에러 초기화 기준 onBlur -> onChange
    
    * feat: useForm반환값 props 객체로 넘기기
    
    * feat: size prop 추가
    
    * feat: maxLength 조건 별도로 분리
    
    * chore: 불필요한 Omit 제거
    
    * chore: 불필요한 Omit 제거
    
    * feat: type 속성 추가
    
    * fix: classList->stylecomposition 방식으로 변경
    
    * fix: styleVariant 적용 및 htmlAttr 사용 확대
    
    * fix: inputElementProps spread 연산자 적용
    
    * fix: 합성컴포넌트패턴 및 useContext 적용
    
    * feat: 에러 텍스트 명시 고도화
    
    * fix: clearError onFocus -> onChange로 변경
    
    * fix: 에러 체크 기준 수정사항 반영
    
    * fix: 타입에러 모두 해결
    
    * fix: 코드리뷰 반영
    
    * [Feat] SelectBox 공통 컴포넌트 구현 (#27)
    
    * design: 1차 퍼블리싱
    
    * feat: radio input 활용 및 button과 연결
    
    * feat: 선택한 값 버튼에 연동 및 추가 스타일링
    
    * design: 세부 스타일링
    
    * feat: default / selected 조건부 스타일링
    
    * fix: default / selected 조건부 스타일링 문제 해결
    
    * feat: 필수 항목 여부 에러처리 구현
    
    * feat: 공통 컴포넌트 묶는 작업
    
    * design: hover 추가속성 추가
    
    * fix: styleVariant 적용
    
    * fix: hover background 추가
    
    * fix: css 수정사항 반영
    
    * fix: formColors common constants로 분리
    
    * fix: styleVary에 focusShadow 추가
    
    * fix: merge
    
    * fix: 타입 에러 해결
    
    * fix: 데이터 관리 형태 수정
    
    * fix: 코드리뷰 반영

--- src/common/components/Input/style.css.ts
+++ src/common/components/Input/style.css.ts
@@ -1,3 +1,4 @@
+import { formColors } from '@constants/styleValues';
 import { style, styleVariants } from '@vanilla-extract/css';
 
 import { theme } from 'styles/theme.css';
@@ -64,18 +65,15 @@ const input = style({
   },
 });
 
-export const inputVar = styleVariants(
+export const inputVar = styleVariants(formColors, ({ boxShadow, focusShadow }) => [
+  input,
   {
-    default: theme.color.border,
-    error: theme.color.error,
-  },
-  (borderColor) => [
-    input,
-    {
-      boxShadow: `0 0 0 1px ${borderColor} inset`,
+    boxShadow,
+    ':focus': {
+      boxShadow: focusShadow,
     },
-  ],
-);
+  },
+]);
 
 const description = style({
   display: 'flex',
