7011ae9b09a769fb33295624a8463677dd6ee606
lydiacho <sheecho0804@ewhain.net>
2024-06-17 22:19:29 +0900

    init: Footer 작업 세팅

--- src/common/components/Layout/index.tsx
+++ src/common/components/Layout/index.tsx
@@ -1,12 +1,16 @@
 import { PropsWithChildren } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Footer from '@components/Footer';
+
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
       <header>header입니다.</header>
       <main>{children || <Outlet />}</main>
-      <footer>footer입니다.</footer>
+      <footer>
+        <Footer />
+      </footer>
     </>
   );
 };
