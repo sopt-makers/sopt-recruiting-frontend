e32a02466b3a47e984641eea8129913913125e84
lydiacho <sheecho0804@ewhain.net>
2024-06-17 23:49:00 +0900

    fix: 헤더 로고 장식용

--- src/common/components/Header/index.tsx
+++ src/common/components/Header/index.tsx
@@ -1,6 +1,3 @@
-import { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
-
 import NowsoptLogo from '@assets/NowsoptLogo';
 
 import { menuItems } from './contants';
@@ -8,17 +5,9 @@ import MenuItem from './MenuItem';
 import { container, menuList } from './style.css';
 
 const Header = () => {
-  const navigate = useNavigate();
-  const handleClickLogo = useCallback(() => {
-    // 클릭 액션 확정 후 수정 예정
-    navigate('/');
-  }, [navigate]);
-
   return (
     <header className={container}>
-      <button type="button" onClick={handleClickLogo}>
-        <NowsoptLogo />
-      </button>
+      <NowsoptLogo />
       <nav>
         <ul className={menuList}>
           {menuItems.map(({ text, path }) => (
