fcfe31130e953fa96bd0f8ce40aecfee33627b92
lydiacho <81505421+lydiacho@users.noreply.github.com>
2024-06-18 18:08:00 +0900

    [Design] Footer 구현 (#40)
    * init: Footer 작업 세팅
    
    * design: Footer 퍼블리싱
    
    * design: 디렉토리 이동 및 채널 icons 배치
    
    * feat: 채널 버튼 link 추가
    
    * design: 회칙 텍스트과 아이콘 세로 중앙 정렬
    
    * fix: 코드리뷰 반영

--- src/common/components/Layout/index.tsx
+++ src/common/components/Layout/index.tsx
@@ -1,12 +1,13 @@
 import { PropsWithChildren } from 'react';
 import { Outlet } from 'react-router-dom';
 
+import Footer from '@components/Layout/Footer';
+
 const Layout = ({ children }: PropsWithChildren) => {
   return (
     <>
-      <header>header입니다.</header>
       <main>{children || <Outlet />}</main>
-      <footer>footer입니다.</footer>
+      <Footer />
     </>
   );
 };
