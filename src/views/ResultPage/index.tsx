3765c85de36c9fc3d0d6a77294db068c9dda94e6
Jeon Eonseok <jseokeon@gmail.com>
2024-06-21 18:12:06 +0900

    feat: result page 진입시 dark mode로 변경

--- src/views/ResultPage/index.tsx
+++ src/views/ResultPage/index.tsx
@@ -1,4 +1,14 @@
+import { useContext, useEffect } from 'react';
+
+import { ThemeContext } from '@store/theme-context';
+
 const ResultPage = () => {
+  const { handleDarkMode } = useContext(ThemeContext);
+
+  useEffect(() => {
+    handleDarkMode();
+  }, [handleDarkMode]);
+
   return <div>ResultPage</div>;
 };
 
