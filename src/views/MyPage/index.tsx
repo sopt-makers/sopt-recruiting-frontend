0078a316be86ccd88c94a0b752ac9e1e0e9a5d0e
Eonseok Jeon <121864459+eonseok-jeon@users.noreply.github.com>
2024-06-22 00:51:33 +0900

    [Design] 마이페이지 퍼블리싱 (#46)
    * fix: 헤더 길이 고정
    
    * design: 기본 틀 작성
    
    * design: container css 작성
    
    * design: my info 부분 css 구현
    
    * docs: gitkeep 삭제
    
    * design: font 추가
    
    * fix: px 수정

--- src/views/MyPage/index.tsx
+++ src/views/MyPage/index.tsx
@@ -1,5 +1,35 @@
+import Button from '@components/Button';
+import Callout from '@components/Callout';
+import Title from '@components/Title';
+
+import { container, infoContainer, infoLabel, infoValue, itemWrapper, lastItemWrapper } from './style.css';
+
+const MyInfoItem = ({ label, value }: { label: string; value: string }) => {
+  return (
+    <li className={itemWrapper}>
+      <span className={infoLabel}>{label}</span>
+      <span className={infoValue}>{value}</span>
+    </li>
+  );
+};
+
 const MyPage = () => {
-  return <div>MyPage</div>;
+  return (
+    <section className={container}>
+      <Title>지원 현황</Title>
+      <Callout>{`지원서는 면접 이후부터 열람이 불가하오니\n백업해두시길 바랍니다.`}</Callout>
+      <ol className={infoContainer}>
+        <MyInfoItem label="기수" value="34기" />
+        <MyInfoItem label="이름" value="김솝트" />
+        <MyInfoItem label="지원파트" value="디자인" />
+        <MyInfoItem label="지원상태" value="지원 완료" />
+        <li className={lastItemWrapper}>
+          <span className={infoLabel}>지원서</span>
+          <Button padding="15x25">지원서 확인</Button>
+        </li>
+      </ol>
+    </section>
+  );
 };
 
 export default MyPage;
--- src/views/MyPage/style.css.ts
+++ src/views/MyPage/style.css.ts
@@ -1,3 +1,48 @@
 import { style } from '@vanilla-extract/css';
 
 import { theme } from 'styles/theme.css';
+
+export const container = style({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: 50,
+  justifyContent: 'center',
+  margin: 95,
+});
+
+export const infoContainer = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between',
+  width: 467,
+  height: 455,
+  padding: '38px 80px',
+  border: `1px solid ${theme.color.border}`,
+  borderRadius: 18,
+});
+
+export const itemWrapper = style({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+});
+
+export const infoLabel = style({
+  width: 63,
+  color: theme.color.lighterText,
+  ...theme.font.BODY_1_18_M,
+});
+
+export const infoValue = style({
+  width: 132,
+  color: theme.color.baseText,
+  textAlign: 'center',
+  ...theme.font.TITLE_5_18_SB,
+});
+
+export const lastItemWrapper = style([
+  itemWrapper,
+  {
+    marginTop: '-15px',
+  },
+]);
