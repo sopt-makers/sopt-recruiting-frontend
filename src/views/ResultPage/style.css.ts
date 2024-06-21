e7f8bc661146bb11cb0ff2fceae5e1b935177c96
Jeon Eonseok <jseokeon@gmail.com>
2024-06-21 20:57:57 +0900

    design: css 구현

--- src/views/ResultPage/style.css.ts
+++ src/views/ResultPage/style.css.ts
@@ -1,3 +1,31 @@
 import { style } from '@vanilla-extract/css';
+import { calc } from '@vanilla-extract/css-utils';
 
 import { theme } from 'styles/theme.css';
+
+export const container = style({
+  position: 'relative',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+  width: '100%',
+  height: calc.subtract('100vh', '74px'),
+  minHeight: 700,
+  overflow: 'hidden',
+});
+
+export const contentWrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  width: 720,
+  gap: 50,
+  color: theme.color.baseText,
+});
+
+export const content = style({
+  whiteSpace: 'pre-line',
+});
+
+export const strongText = style({
+  color: 'red',
+});
