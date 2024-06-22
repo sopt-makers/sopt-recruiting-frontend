fa48b94faa1e3d4ebaeb6d7e348ef3d4d1fe189b
Jeon Eonseok <jseokeon@gmail.com>
2024-06-22 11:22:54 +0900

    design: 비밀번호 재설정 모달 구현

---
+++ src/views/PasswordPage/components/CompleteDialog/index.tsx
@@ -0,0 +1,24 @@
+import { forwardRef } from 'react';
+import { Link } from 'react-router-dom';
+
+import Dialog from '@components/Dialog';
+
+import { buttonInside, buttonOutside, mainText, subText } from './style.css';
+
+const CompleteDialog = forwardRef<HTMLDialogElement>((_, ref) => {
+  return (
+    <Dialog ref={ref}>
+      <p className={mainText}>비밀먼호 재설정이 완료되었어요.</p>
+      <p className={subText}>&apos;지원하기&apos; 페이지로 이동할게요.</p>
+      <form method="dialog" className={buttonOutside}>
+        <Link to="/apply" className={buttonInside}>
+          확인
+        </Link>
+      </form>
+    </Dialog>
+  );
+});
+
+CompleteDialog.displayName = 'CompleteDialog';
+
+export default CompleteDialog;
