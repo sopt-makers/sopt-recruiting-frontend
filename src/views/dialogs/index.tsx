ec7721e14f5d7cbeb42fece2902c2584d88ca368
Jeon Eonseok <jseokeon@gmail.com>
2024-06-22 13:03:56 +0900

    refactor: dialog 하나의 폴더에서 관리

---
+++ src/views/dialogs/index.tsx
@@ -0,0 +1,4 @@
+export { default as CompleteDialog } from './CompleteDialog';
+export { default as DraftDialog } from './DraftDialog';
+export { default as ExitDialog } from './ExitDialog';
+export { default as SubmitDialog } from './SubmitDialog';
