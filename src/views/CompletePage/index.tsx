abdd462a2f9e001118cb333b7ac72dd197dc8508
Eonseok Jeon <121864459+eonseok-jeon@users.noreply.github.com>
2024-06-22 12:29:00 +0900

    [Design] 지원 완료 페이지 퍼블리싱 (#49)
    * feat: 기본 틀 잡기
    
    * design: icon 추가
    
    * design: css 구현
    
    * fix: 화면 중앙에 컨텐츠 배치
    
    * docs: gitkeep 제거

--- src/views/CompletePage/index.tsx
+++ src/views/CompletePage/index.tsx
@@ -1,5 +1,27 @@
+import Button from '@components/Button';
+import Callout from '@components/Callout';
+
+import IconCheckmark from './icons/IconCheckmark';
+import { container, icon, mainText, subText } from './style.css';
+
 const CompletePage = () => {
-  return <div>CompletePage</div>;
+  const name = '000';
+  const th = '00';
+
+  return (
+    <section className={container}>
+      <div className={icon}>
+        <IconCheckmark />
+      </div>
+      <p className={mainText}>{`${name}님의\n${th}기 지원서가 접수되었습니다.`}</p>
+      <p className={subText}>이메일로 지원 접수 완료 알림이 발송되었습니다.</p>
+      <Callout
+        style={{
+          marginBottom: '50px',
+        }}>{`이메일 도착 시점에 차이가 있을 수 있습니다.\n이메일이 오지 않으면 스팸 메일함을 확인해주세요.`}</Callout>
+      <Button>마이페이지로 이동하기</Button>
+    </section>
+  );
 };
 
 export default CompletePage;
