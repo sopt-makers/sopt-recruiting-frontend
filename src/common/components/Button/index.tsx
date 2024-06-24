36e8e10b64588466ad6252e5338c10bce8d90d37
Jeon Eonseok <jseokeon@gmail.com>
2024-06-24 12:39:40 +0900

    feat: Button 컴포넌트를 Link로 변경가능하도록 수정

--- src/common/components/Button/index.tsx
+++ src/common/components/Button/index.tsx
@@ -1,12 +1,16 @@
+import { LinkProps, To } from 'react-router-dom';
+
 import { container, outsideBox, paddings } from './style.css';
 
-import type { ButtonHTMLAttributes, ReactNode } from 'react';
+import type { ButtonHTMLAttributes, ForwardRefExoticComponent, ReactNode, RefAttributes } from 'react';
 
-interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement | HTMLAnchorElement> {
   children?: ReactNode;
   className?: string;
-  padding?: '15x32' | '13x20' | '10x24' | '15x25';
   buttonStyle?: 'solid' | 'line';
+  padding?: '15x32' | '13x20' | '10x24' | '15x25';
+  to?: To;
+  Tag?: ForwardRefExoticComponent<LinkProps & RefAttributes<HTMLAnchorElement>> | 'button';
 }
 
 const Button = ({
@@ -14,18 +18,21 @@ const Button = ({
   className,
   buttonStyle = 'solid',
   padding = '15x32',
+  to,
+  Tag = 'button',
   ...buttonElementProps
 }: ButtonProps) => {
   const { disabled, type = 'button' } = buttonElementProps;
 
   return (
     <div className={`${className} ${outsideBox[disabled ? 'disabled' : buttonStyle]}`}>
-      <button
+      <Tag
+        to={to as To}
         type={type}
         className={`${container[buttonStyle]} ${paddings[padding]} ${className}`}
         {...buttonElementProps}>
         {children}
-      </button>
+      </Tag>
     </div>
   );
 };
