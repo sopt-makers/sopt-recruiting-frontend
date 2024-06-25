4903f298ab7e4e8bb657355f45f74a1660c3e354
lydiacho <sheecho0804@ewhain.net>
2024-06-26 01:02:14 +0900

    fix: defaultValues 수정

--- src/common/constants/defaultValues.ts
+++ src/common/constants/defaultValues.ts
@@ -1,16 +1,16 @@
 export type TFormValues = Record<string, string | boolean>;
 
 export const defaultValues: TFormValues = {
-  이름: '',
+  이름: '김솝트', // 서버 데이터
   성별: '성별을 선택해주세요',
   생년월일: '',
-  연락처: '',
-  이메일: '',
+  연락처: '010-1234-5678', // 서버 데이터
+  이메일: 'qwer@naver.com', // 서버 데이터
   거주지: '',
   지하철역: '',
   학교: '',
   재학여부: true,
   학과: '',
-  학년: '',
+  학년: '학년을 선택해주세요',
   이전기수활동여부: '이전 기수 활동 여부를 선택해주세요',
 };
