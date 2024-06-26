380f7c69e3635d89818bdd9e4235eabc9e72e669
Jeon Eonseok <jseokeon@gmail.com>
2024-06-26 22:15:22 +0900

    fix: 브라우저 자체 required 에러 문구 띄우는 거 제거

--- src/common/components/Input/InputTheme.tsx
+++ src/common/components/Input/InputTheme.tsx
@@ -10,7 +10,7 @@ import { TextBoxProps } from './types';
 export const TextBox이름 = ({ formObject }: Pick<TextBoxProps, 'formObject'>) => {
   return (
     <TextBox label="이름" formObject={formObject} required>
-      <InputLine label="이름" placeholder="지원자 이름을 공백 없이 입력해주세요." required />
+      <InputLine label="이름" placeholder="지원자 이름을 공백 없이 입력해주세요." />
     </TextBox>
   );
 };
@@ -25,7 +25,7 @@ export const TextBox이메일 = ({ formObject }: Pick<TextBoxProps, 'formObject'
 
   return (
     <TextBox label="이메일" formObject={formObject} required>
-      <InputLine label="이메일" placeholder="이메일을 입력해주세요." required type="email">
+      <InputLine label="이메일" placeholder="이메일을 입력해주세요." type="email">
         <InputButton text="이메일 인증" onClick={handleClick이메일인증} disabled={isActive} />
         <Timer
           isActive={isActive}
@@ -34,7 +34,7 @@ export const TextBox이메일 = ({ formObject }: Pick<TextBoxProps, 'formObject'
           }}
         />
       </InputLine>
-      <InputLine label="인증번호" placeholder="이메일 인증 번호를 작성해주세요" required>
+      <InputLine label="인증번호" placeholder="이메일 인증 번호를 작성해주세요">
         <InputButton text="확인" onClick={handleClick확인} disabled />
       </InputLine>
     </TextBox>
@@ -47,13 +47,8 @@ export const TextBox비밀번호 = ({ formObject }: Pick<TextBoxProps, 'formObje
 
   return (
     <TextBox label={textVar} formObject={formObject} required>
-      <InputLine label={textVar} placeholder={`${textVar}를 입력해주세요.`} required type="password" />
-      <InputLine
-        label="비밀번호 재확인"
-        placeholder="비밀번호 확인을 위해 다시 한번 입력해주세요."
-        required
-        type="password"
-      />
+      <InputLine label={textVar} placeholder={`${textVar}를 입력해주세요.`} type="password" />
+      <InputLine label="비밀번호 재확인" placeholder="비밀번호 확인을 위해 다시 한번 입력해주세요." type="password" />
     </TextBox>
   );
 };
