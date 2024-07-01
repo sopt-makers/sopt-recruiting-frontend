81d837abe05e9d948ebe69501b2703c04210ea09
Jeon Eonseok <jseokeon@gmail.com>
2024-07-01 16:32:39 +0900

    feat: GNB link값 수정

--- src/common/components/Layout/Header/contants.ts
+++ src/common/components/Layout/Header/contants.ts
@@ -1,15 +1,24 @@
-export const MENU_ITEMS = [
+type menuItemsTypes = {
+  text: string;
+  path?: string;
+  target?: '_blank' | '_self';
+};
+
+export const MENU_ITEMS: menuItemsTypes[] = [
   {
     text: '모집공고',
-    path: '/apply',
+    path: 'https://www.sopt.org/recruit',
+    target: '_blank',
   },
   {
     text: '문의하기',
-    path: '/sign-up',
+    path: 'https://pf.kakao.com/_JdTKd',
+    target: '_blank',
   },
   {
     text: '로그인',
     path: '/',
+    target: '_self',
   },
   {
     text: '김솝트님',
