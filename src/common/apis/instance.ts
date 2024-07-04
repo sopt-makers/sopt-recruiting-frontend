a2f33dc08d725ae1e3bedb9138630d60d2c09a5d
Jeon Eonseok <jseokeon@gmail.com>
2024-07-04 22:29:35 +0900

    chore: api 통신을 위한 기본 설정

---
+++ src/common/apis/instance.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: import.meta.env.VITE_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+export default instance;
