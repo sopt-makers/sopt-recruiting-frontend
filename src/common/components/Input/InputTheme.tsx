4e56d45fe88a2624fcd41bdd83cef4aeb2c86d64
Jeon Eonseok <jseokeon@gmail.com>
2024-07-04 21:03:04 +0900

    chore: 마침표 추가

--- src/common/components/Input/InputTheme.tsx
+++ src/common/components/Input/InputTheme.tsx
@@ -99,7 +99,7 @@ export const TextBox이메일 = ({ formObject, isVerificationSuccess, onVerifica
       <InputLine
         disabled={!isActive}
         label="인증번호"
-        placeholder="이메일 인증 번호를 작성해주세요"
+        placeholder="이메일 인증 번호를 작성해주세요."
         maxLength={VALIDATION_CHECK.verificationCode.maxLength}>
         <InputButton
           disabled={!isActive}
