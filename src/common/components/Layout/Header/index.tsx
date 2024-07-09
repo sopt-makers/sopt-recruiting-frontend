dd378ddb2774fb125e05034dbb96713fcfc0a545
Jeon Eonseok <jseokeon@gmail.com>
2024-07-10 01:46:29 +0900

    feat: header에 동적으로 이름 렌더링

--- src/common/components/Layout/Header/index.tsx
+++ src/common/components/Layout/Header/index.tsx
@@ -1,10 +1,18 @@
+import { useContext } from 'react';
+
 import NowsoptLogo from '@assets/NowsoptLogo';
+import { UserInfoContext, UserInfoContextType } from '@store/userInfoContext';
 
 import { MENU_ITEMS } from './contants';
 import MenuItem from './MenuItem';
 import { container, menuList } from './style.css';
 
 const Header = () => {
+  const isSignedIn = localStorage.getItem('soptApplyAccessToken');
+  const {
+    userInfo: { name },
+  }: UserInfoContextType = useContext(UserInfoContext);
+
   return (
     <header className={container}>
       <NowsoptLogo />
@@ -13,6 +21,11 @@ const Header = () => {
           {MENU_ITEMS.map(({ text, path, target }) => (
             <MenuItem key={text} text={text} path={path} target={target} />
           ))}
+          {isSignedIn ? (
+            <MenuItem key="로그인완료" text={`${name}님`} />
+          ) : (
+            <MenuItem key="로그인" text="로그인" path="/" />
+          )}
         </ul>
       </nav>
     </header>
