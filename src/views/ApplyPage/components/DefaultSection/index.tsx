84bed89447b0d6432940a36c4396258c4c7801a0
Jeon Eonseok <jseokeon@gmail.com>
2024-07-10 16:17:41 +0900

    chore: placeholder 재재수정

--- src/views/ApplyPage/components/DefaultSection/index.tsx
+++ src/views/ApplyPage/components/DefaultSection/index.tsx
@@ -126,7 +126,7 @@ const DefaultSection = ({
       <TextBox label="지하철역" formObject={formObject} required size="lg">
         <InputLine
           label="지하철역"
-          placeholder="역의 이름을 정확하게 적어주세요. (ex. ㅇㅇ역)"
+          placeholder="역의 이름을 정확하게 적어주세요. (ex. &#9675;&#9675;역)"
           maxLength={VALIDATION_CHECK.subway.maxLength}
           pattern={VALIDATION_CHECK.subway.pattern}
           errorText={VALIDATION_CHECK.subway.errorText}
@@ -136,7 +136,7 @@ const DefaultSection = ({
         <TextBox label="학교" formObject={formObject} required size="sm">
           <InputLine
             label="학교"
-            placeholder="학교 이름을 정확하게 적어주세요. (ex. ㅇㅇ대학교)"
+            placeholder="학교 이름을 정확하게 적어주세요. (ex. &#9675;&#9675;대학교)"
             maxLength={VALIDATION_CHECK.textInput.maxLength}
             pattern={VALIDATION_CHECK.textInput.pattern}
             errorText={VALIDATION_CHECK.textInput.errorText}
@@ -150,7 +150,7 @@ const DefaultSection = ({
         <TextBox label="학과" formObject={formObject} required size="sm">
           <InputLine
             label="학과"
-            placeholder="학과 이름을 정확하게 적어주세요. (ex. ㅇㅇ학과)"
+            placeholder="학과 이름을 정확하게 적어주세요. (ex. &#9675;&#9675;학과)"
             maxLength={VALIDATION_CHECK.textInput.maxLength}
             pattern={VALIDATION_CHECK.textInput.pattern}
             errorText={VALIDATION_CHECK.textInput.errorText}
