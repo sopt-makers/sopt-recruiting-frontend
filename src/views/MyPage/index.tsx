61bbd36c2e7e3184c29dec8f796aef37468b5ca4
Eonseok Jeon <121864459+eonseok-jeon@users.noreply.github.com>
2024-07-13 00:55:45 +0900

    [Feat] 마이 페이지 api 연동 (#113)
    * chore: api 통신을 위한 기본 설정
    
    * feat: token 여부 체크하는 새로운 instance 생성
    
    * fix: token 전달법 수정
    
    * feat: my page api 연결
    
    * fix: type error 해결
    
    * feat: 지원상태에 따른 조건부 렌더링
    
    * chore: 주석 제거

--- src/views/MyPage/index.tsx
+++ src/views/MyPage/index.tsx
@@ -1,28 +1,61 @@
+import { useQuery } from '@tanstack/react-query';
+import { AxiosError, AxiosResponse } from 'axios';
+
 import Button from '@components/Button';
 import Callout from '@components/Callout';
 import Title from '@components/Title';
+import BigLoading from 'views/loadings/BigLoding';
 
+import { getMyInfo } from './apis';
 import { container, infoContainer, infoLabel, infoValue, itemWrapper, lastItemWrapper } from './style.css';
+import { MyError, MyResponse } from './types';
 
-const MyInfoItem = ({ label, value }: { label: string; value: string }) => {
+const MyInfoItem = ({ label, value }: { label: string; value: string | number | boolean }) => {
+  let renderValue = value;
+
+  switch (label) {
+    case '지원파트':
+      renderValue = value ? value : '미지원';
+      break;
+    case '지원상태':
+      renderValue = value ? '지원완료' : '미지원';
+      break;
+    default:
+      break;
+  }
+
   return (
     <li className={itemWrapper}>
       <span className={infoLabel}>{label}</span>
-      <span className={infoValue}>{value}</span>
+      <span className={infoValue}>{renderValue}</span>
     </li>
   );
 };
 
 const MyPage = () => {
+  const { data, isLoading } = useQuery<
+    AxiosResponse<MyResponse, null>,
+    AxiosError<MyError, null>,
+    AxiosResponse<MyResponse, null>,
+    string[]
+  >({
+    queryKey: ['my'],
+    queryFn: getMyInfo,
+  });
+
+  if (isLoading) return <BigLoading />;
+
+  const { season, name, part, submit } = data!.data;
+
   return (
     <section className={container}>
       <Title>지원 현황</Title>
       <Callout>{`지원서는 면접 이후부터 열람이 불가하오니\n백업해두시길 바랍니다.`}</Callout>
       <ol className={infoContainer}>
-        <MyInfoItem label="기수" value="34기" />
-        <MyInfoItem label="이름" value="김솝트" />
-        <MyInfoItem label="지원파트" value="디자인" />
-        <MyInfoItem label="지원상태" value="지원 완료" />
+        <MyInfoItem label="기수" value={season} />
+        <MyInfoItem label="이름" value={name} />
+        <MyInfoItem label="지원파트" value={part} />
+        <MyInfoItem label="지원상태" value={submit} />
         <li className={lastItemWrapper}>
           <span className={infoLabel}>지원서</span>
           <Button padding="15x25">지원서 확인</Button>
