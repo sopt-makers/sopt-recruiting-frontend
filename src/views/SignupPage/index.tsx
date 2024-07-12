0a8b833d3372c7247ab2ab1535de544cc8fd102e
Jeon Eonseok <jseokeon@gmail.com>
2024-07-13 02:43:07 +0900

    feat: 이미 존재하는 이메일 존재 시 이메일에 자동 포커스

--- src/views/SignupPage/index.tsx
+++ src/views/SignupPage/index.tsx
@@ -1,5 +1,6 @@
 import { useMutation } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
+import { useEffect } from 'react';
 import { FieldValues, useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 
@@ -61,6 +62,13 @@ const SignupPage = () => {
       group: 'OB',
     });
   };
+
+  useEffect(() => {
+    if (formObject.formState.errors['이메일']?.message === VALIDATION_CHECK.email.errorTextExistence) {
+      formObject.setFocus('이메일');
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [formObject.formState.errors['이메일']?.message, formObject.setFocus]);
 
   return (
     <form noValidate onSubmit={handleSubmit(handleSubmitSignUp)} className={container}>
