ecb0f87c350e2797cbad6b4c559bcac444c454e0
Jeon Eonseok <jseokeon@gmail.com>
2024-07-15 21:12:17 +0900

    fix: 무한 렌더링 에러 해결

--- src/views/MainPage/index.tsx
+++ src/views/MainPage/index.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from '@tanstack/react-query';
 import { AxiosError, AxiosResponse } from 'axios';
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 
 import { UserInfoContext } from '@store/userInfoContext';
 import ApplyPage from 'views/ApplyPage';
@@ -14,7 +14,7 @@ const MainPage = () => {
   const { handleSaveUserInfo } = useContext(UserInfoContext);
   const isSignedIn = localStorage.getItem('soptApplyAccessToken');
 
-  const { isLoading } = useQuery<
+  const { data, isLoading } = useQuery<
     AxiosResponse<RecruitingResponse, null>,
     AxiosError<RecruitingError, null>,
     AxiosResponse<RecruitingResponse, null>,
@@ -22,52 +22,51 @@ const MainPage = () => {
   >({
     queryKey: ['get-recruiting-info'],
     queryFn: getRecruitingInfo,
-    select: (data) => {
-      const {
-        name,
-        season,
-        group,
-        ybApplicationStart,
-        obApplicationStart,
-        ybApplicationEnd,
-        obApplicationEnd,
-        ybApplicationConfirmStart,
-        obApplicationConfirmStart,
-        ybApplicationConfirmEnd,
-        obApplicationConfirmEnd,
-        ybApplicationPassConfirmStart,
-        obApplicationPassConfirmStart,
-        ybApplicationPassConfirmEnd,
-        obApplicationPassConfirmEnd,
-        ybFinalPassConfirmStart,
-        obFinalPassConfirmStart,
-        ybFinalPassConfirmEnd,
-        obFinalPassConfirmEnd,
-        ybInterviewStart,
-        obInterviewStart,
-        ybInterviewEnd,
-        obInterviewEnd,
-      } = data.data.season;
-
-      handleSaveUserInfo({
-        name,
-        season,
-        group,
-        applicationStart: group === 'YB' ? ybApplicationStart : obApplicationStart, // 서류 지원 시작
-        applicationEnd: group === 'YB' ? ybApplicationEnd : obApplicationEnd, // 서류 지원 끝
-        applicationConfirmStart: group === 'YB' ? ybApplicationConfirmStart : obApplicationConfirmStart, // 서류 지원 확인 시작
-        applicationConfirmEnd: group === 'YB' ? ybApplicationConfirmEnd : obApplicationConfirmEnd, // 서류 지원 확인 종료
-        applicationPassConfirmStart: group === 'YB' ? ybApplicationPassConfirmStart : obApplicationPassConfirmStart, // 서류 합격 확인 시작
-        applicationPassConfirmEnd: group === 'YB' ? ybApplicationPassConfirmEnd : obApplicationPassConfirmEnd, // 서류 합격 확인 종료
-        finalPassConfirmStart: group === 'YB' ? ybFinalPassConfirmStart : obFinalPassConfirmStart, // 최종 합격 확인 시작
-        finalPassConfirmEnd: group === 'YB' ? ybFinalPassConfirmEnd : obFinalPassConfirmEnd, // 최종 합격 확인 종료
-        interviewStart: group === 'YB' ? ybInterviewStart : obInterviewStart, // 면접 시작
-        interviewEnd: group === 'YB' ? ybInterviewEnd : obInterviewEnd, // 면접 끝
-      });
-
-      return data;
-    },
   });
+
+  useEffect(() => {
+    const {
+      name,
+      season,
+      group,
+      ybApplicationStart,
+      obApplicationStart,
+      ybApplicationEnd,
+      obApplicationEnd,
+      ybApplicationConfirmStart,
+      obApplicationConfirmStart,
+      ybApplicationConfirmEnd,
+      obApplicationConfirmEnd,
+      ybApplicationPassConfirmStart,
+      obApplicationPassConfirmStart,
+      ybApplicationPassConfirmEnd,
+      obApplicationPassConfirmEnd,
+      ybFinalPassConfirmStart,
+      obFinalPassConfirmStart,
+      ybFinalPassConfirmEnd,
+      obFinalPassConfirmEnd,
+      ybInterviewStart,
+      obInterviewStart,
+      ybInterviewEnd,
+      obInterviewEnd,
+    } = data?.data.season || {};
+
+    handleSaveUserInfo({
+      name,
+      season,
+      group,
+      applicationStart: group === 'YB' ? ybApplicationStart : obApplicationStart, // 서류 지원 시작
+      applicationEnd: group === 'YB' ? ybApplicationEnd : obApplicationEnd, // 서류 지원 끝
+      applicationConfirmStart: group === 'YB' ? ybApplicationConfirmStart : obApplicationConfirmStart, // 서류 지원 확인 시작
+      applicationConfirmEnd: group === 'YB' ? ybApplicationConfirmEnd : obApplicationConfirmEnd, // 서류 지원 확인 종료
+      applicationPassConfirmStart: group === 'YB' ? ybApplicationPassConfirmStart : obApplicationPassConfirmStart, // 서류 합격 확인 시작
+      applicationPassConfirmEnd: group === 'YB' ? ybApplicationPassConfirmEnd : obApplicationPassConfirmEnd, // 서류 합격 확인 종료
+      finalPassConfirmStart: group === 'YB' ? ybFinalPassConfirmStart : obFinalPassConfirmStart, // 최종 합격 확인 시작
+      finalPassConfirmEnd: group === 'YB' ? ybFinalPassConfirmEnd : obFinalPassConfirmEnd, // 최종 합격 확인 종료
+      interviewStart: group === 'YB' ? ybInterviewStart : obInterviewStart, // 면접 시작
+      interviewEnd: group === 'YB' ? ybInterviewEnd : obInterviewEnd, // 면접 끝
+    });
+  }, [data, handleSaveUserInfo]);
 
   if (isLoading) return <BigLoading />;
 
