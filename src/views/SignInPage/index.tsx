3376a09dbadeefc6bd7c7ae0ee7031c79210485b
Jeon Eonseok <jseokeon@gmail.com>
2024-07-17 21:38:59 +0900

    fix: 빠진 validation 추가

--- src/views/SignInPage/index.tsx
+++ src/views/SignInPage/index.tsx
@@ -75,10 +75,24 @@ const SignInPage = () => {
         </p>
       </Callout>
       <TextBox label="이메일" name="email" formObject={formObject} required>
-        <InputLine name="email" placeholder="이메일을 입력해주세요" type="email" />
+        <InputLine
+          name="email"
+          placeholder="이메일을 입력해주세요"
+          type="email"
+          pattern={VALIDATION_CHECK.email.pattern}
+          maxLength={VALIDATION_CHECK.email.maxLength}
+          errorText={VALIDATION_CHECK.email.errorText}
+        />
       </TextBox>
       <TextBox label="비밀번호" name="password" formObject={formObject} required>
-        <InputLine name="password" placeholder="비밀번호를 입력해주세요" type="password" />
+        <InputLine
+          name="password"
+          placeholder="비밀번호를 입력해주세요"
+          type="password"
+          pattern={VALIDATION_CHECK.password.pattern}
+          maxLength={VALIDATION_CHECK.password.maxLength}
+          errorText={VALIDATION_CHECK.password.errorText}
+        />
         <Description>
           <p>비밀번호를 잃어버리셨나요?</p>
           <Link className={newPasswordButton} to="/password">
