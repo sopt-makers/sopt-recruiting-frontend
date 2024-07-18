692e465c11fd2fab44eeb36741d3e1f1a95a1be9
Jeon Eonseok <jseokeon@gmail.com>
2024-07-19 00:49:33 +0900

    chore: 불필요한 코드 제거

--- src/common/components/Layout/Header/index.tsx
+++ src/common/components/Layout/Header/index.tsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 import NowsoptLogo from '@assets/NowsoptLogo';
 import { RecruitingInfoContext } from '@store/recruitingInfoContext';
