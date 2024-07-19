6f8da8060845346dd2615a4b415794a8c0bdf958
Jeon Eonseok <jseokeon@gmail.com>
2024-07-19 11:35:25 +0900

    fix: 면접 끝날 때 까지 지원서 확인 가능하도록 수정

--- src/common/hooks/useDate.tsx
+++ src/common/hooks/useDate.tsx
@@ -24,10 +24,10 @@ const useDate = () => {
     obFinalPassConfirmStart,
     ybFinalPassConfirmEnd,
     obFinalPassConfirmEnd,
-    ybInterviewStart,
-    obInterviewStart,
-    // ybInterviewEnd,
-    // obInterviewEnd,
+    // ybInterviewStart,
+    // obInterviewStart,
+    ybInterviewEnd,
+    obInterviewEnd,
   } = data?.data.season || {};
 
   const applicationStart = group === 'YB' ? ybApplicationStart : obApplicationStart; // 서류 시작
@@ -36,8 +36,8 @@ const useDate = () => {
   // const applicationConfirmEndValue = group === 'YB' ? ybApplicationConfirmEnd : obApplicationConfirmEnd; // 서류 검사 마감
   const applicationPassConfirmStart = group === 'YB' ? ybApplicationPassConfirmStart : obApplicationPassConfirmStart; // 서류 합격 시작
   const applicationPassConfirmEnd = group === 'YB' ? ybApplicationPassConfirmEnd : obApplicationPassConfirmEnd; // 서류 합격 마감
-  const interviewStart = group === 'YB' ? ybInterviewStart : obInterviewStart; // 면접 시작
-  // const interviewEnd = group === 'YB' ? ybInterviewEnd : obInterviewEnd; // 면접 마감
+  // const interviewStart = group === 'YB' ? ybInterviewStart : obInterviewStart; // 면접 시작
+  const interviewEnd = group === 'YB' ? ybInterviewEnd : obInterviewEnd; // 면접 마감
   const finalPassConfirmStart = group === 'YB' ? ybFinalPassConfirmStart : obFinalPassConfirmStart; // 최종 합격 시작
   const finalPassConfirmEnd = group === 'YB' ? ybFinalPassConfirmEnd : obFinalPassConfirmEnd; // 최종 합격 마감
 
@@ -47,13 +47,13 @@ const useDate = () => {
   const afterApply = isBefore(new Date(applicationEnd || ''), new Date());
   const beforeScreeningResult = isAfter(new Date(applicationPassConfirmStart || ''), new Date());
   const afterScreeningResult = isBefore(new Date(applicationPassConfirmEnd || ''), new Date());
-  const startInterview = isBefore(new Date(interviewStart || ''), new Date());
+  const afterInterview = isBefore(new Date(interviewEnd || ''), new Date());
   const beforeFinalResult = isAfter(new Date(finalPassConfirmStart || ''), new Date());
 
   const NoMoreRecruit = beforeRecruiting || afterRecruiting; // 지원 전
   const NoMoreApply = beforeRecruiting || afterApply; // 서류 지원 마감
   const NoMoreScreeningResult = beforeScreeningResult || afterScreeningResult; // 서류 합불 확인 기간 아님
-  const NoMoreReview = startInterview; // 면접 시작 -> 지원서 확인 불가
+  const NoMoreReview = afterInterview; // 면접 마감 -> 지원서 확인 불가
   const NoMoreFinalResult = beforeFinalResult || afterRecruiting; // 최종 합불 확인 기간 아님
 
   return {
