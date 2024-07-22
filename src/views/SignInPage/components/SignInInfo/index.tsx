3998f5b50001a250770dd264aed7fd70a0b6414a
Jeon Eonseok <jseokeon@gmail.com>
2024-07-23 03:52:40 +0900

    chore: makers인지 파악하는 변수 분리

--- src/views/SignInPage/components/SignInInfo/index.tsx
+++ src/views/SignInPage/components/SignInInfo/index.tsx
@@ -13,10 +13,12 @@ interface SignInInfoProps extends SeasonGroupType {
 }
 
 const SignInInfo = ({ soptName, season, group }: SignInInfoProps) => {
+  const isMakers = soptName?.toLowerCase().includes('makers');
+
   return (
     <>
       <Title>
-        {season}기 {soptName} {soptName === 'Makers' ? '' : group} 지원하기
+        {season}기 {soptName} {isMakers ? '' : group} 지원하기
       </Title>
       <Callout
         Button={
@@ -25,8 +27,8 @@ const SignInInfo = ({ soptName, season, group }: SignInInfoProps) => {
           </Link>
         }>
         <p>
-          {season}기 {soptName} {soptName === 'Makers' ? '' : group} 지원서 작성이 처음이라면 ‘새 지원서 작성하기’를
-          진행해주세요. <strong className={strongText}>이전에 지원서 </strong>를 제출한 적이 있더라도{' '}
+          {season}기 {soptName} {isMakers ? '' : group} 지원서 작성이 처음이라면 ‘새 지원서 작성하기’를 진행해주세요.{' '}
+          <strong className={strongText}>이전에 지원서 </strong>를 제출한 적이 있더라도{' '}
           <strong className={strongText}>반드시</strong> 새 지원서를 작성해야 해요.
         </p>
       </Callout>
