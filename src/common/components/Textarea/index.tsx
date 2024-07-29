87233f82fffad226fd8e80b10f7001ee224ddd1d
Jeon Eonseok <jseokeon@gmail.com>
2024-07-29 19:15:31 +0900

    chore: 이상한 주석 제거

--- src/common/components/Textarea/index.tsx
+++ src/common/components/Textarea/index.tsx
@@ -48,10 +48,3 @@ const Textarea = <T extends FieldValues>({
 };
 
 export default Textarea;
-
-{
-  /* <TextareaHeader maxCount={maxCount} required>
-            {`3. 최근 1년 이내로 머릿속으로만 생각하고 있던 계획을 행동으로 옮겨본 경험이 있나요? 만약 있다면 어떤. 계획이. 었으며, 행동을 통해 어떤 성장을 이루어냈는지에 대해 구체적으로 서술해 주세요. 만약 없다면, 해당 계획을 행동으로 옮기기 위한 액션 플랜을 서술해 주세요.`}
-          </TextareaHeader>
-          <Textarea label="q1" register={register} watch={watch} required errors={errors} maxCount={maxCount} /> */
-}
