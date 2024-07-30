bb02fe192c42f310bd05b4dd6f910721374b19fe
Jeon Eonseok <jseokeon@gmail.com>
2024-07-31 07:26:00 +0900

    fix: 지원서 제출 안 했거나 서류 탈락하면 result page 못 들어가게 막기

--- src/views/ResultPage/index.tsx
+++ src/views/ResultPage/index.tsx
@@ -4,12 +4,15 @@ import useDate from '@hooks/useDate';
 import { ThemeContext } from '@store/themeContext';
 import NoMore from 'views/ErrorPage/components/NoMore';
 import BigLoading from 'views/loadings/BigLoding';
+import useGetMyInfo from 'views/SignedInPage/hooks/useGetMyInfo';
 
 import FinalResult from './components/FinalResult';
 import ScreeningResult from './components/ScreeningResult';
 
 const ResultPage = () => {
   const { handleChangeMode } = useContext(ThemeContext);
+  const { myInfoData, myInfoIsLoading } = useGetMyInfo();
+  const { submit, applicationPass } = myInfoData?.data || {};
 
   const { NoMoreRecruit, NoMoreScreeningResult, NoMoreFinalResult, isLoading, isMakers } = useDate();
 
@@ -21,14 +24,15 @@ const ResultPage = () => {
     };
   }, [handleChangeMode]);
 
-  if (isLoading) return <BigLoading />;
-  if (NoMoreRecruit || (NoMoreScreeningResult && NoMoreFinalResult))
+  if (isLoading || myInfoIsLoading) return <BigLoading />;
+  if (!submit || NoMoreRecruit || (NoMoreScreeningResult && NoMoreFinalResult))
     return <NoMore isMakers={isMakers} content="합불 확인 기간이 아니에요" />;
 
   return (
     <>
       {!NoMoreScreeningResult && <ScreeningResult />}
-      {!NoMoreFinalResult && <FinalResult />}
+      {!NoMoreFinalResult &&
+        (applicationPass ? <FinalResult /> : <NoMore isMakers={isMakers} content="합불 확인 기간이 아니에요" />)}
     </>
   );
 };
