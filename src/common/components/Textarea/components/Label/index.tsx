d58eae0a33f48fb4c1d0e9d64875c6227c25bb33
Jeon Eonseok <jseokeon@gmail.com>
2024-07-30 21:59:11 +0900

    fix: max count 위치 수정

--- src/common/components/Textarea/components/Label/index.tsx
+++ src/common/components/Textarea/components/Label/index.tsx
@@ -1,24 +1,50 @@
+import { Fragment, useId, type HTMLAttributes } from 'react';
+
 import { requireDot, labelStyle } from './style.css';
 
-import type { HTMLAttributes } from 'react';
-
 interface LabelProps extends HTMLAttributes<HTMLHeadingElement> {
-  children: string | number;
+  children: string;
   maxCount: number;
   required: boolean;
   label: string;
 }
 
 const Label = ({ children, maxCount, required, label, ...headerElementProps }: LabelProps) => {
+  const keyId = useId();
+  const questionArray = children.split('\n');
+  const firstEmptyIndex = questionArray.indexOf('');
+
+  const renderQuestions = (questions: string[], limit: number) =>
+    questions.slice(0, limit).map((item, idx) => (
+      <Fragment key={`${item}-${idx}`}>
+        {item}
+        {idx !== limit - 1 && '\n'}
+      </Fragment>
+    ));
+
+  const renderRestQuestions = (questions: string[]) =>
+    questions.slice(firstEmptyIndex).map((item, idx) => {
+      if (item === '') return <br key={`${keyId}-${idx}`} />;
+      return <Fragment key={`${item}-${idx}`}>{item}</Fragment>;
+    });
+
   return (
     <h4 className={labelStyle} {...headerElementProps}>
       <label style={{ cursor: 'pointer' }} htmlFor={label}>
-        <span>{children}</span>
-        <span style={{ position: 'relative' }}>
-          {' '}
-          ({maxCount}자)
-          {required && <i className={requireDot} />}
+        <span>
+          {firstEmptyIndex === -1 ? children : renderQuestions(questionArray, firstEmptyIndex)}
+          <span style={{ position: 'relative' }}>
+            {' '}
+            ({maxCount}자)
+            {required && <i className={requireDot} />}
+          </span>
         </span>
+        {firstEmptyIndex !== -1 && (
+          <>
+            <br />
+            <span>{renderRestQuestions(questionArray)}</span>
+          </>
+        )}
       </label>
     </h4>
   );
