06e00bdbd5702a2a74d484e3b9beb25bbd271c01
lydiacho <sheecho0804@ewhain.net>
2024-08-07 14:44:53 +0900

    design: Description 반응형

--- src/common/components/Input/components/Description/index.tsx
+++ src/common/components/Input/components/Description/index.tsx
@@ -1,10 +1,13 @@
 import { TextBoxProps } from '@components/Input/types';
+import { useDevice } from '@hooks/useDevice';
 
-import { descriptionVar } from './style.css';
+import { descriptionFontVar, descriptionVar } from './style.css';
 
 // TextBox 내부 Input 하단의 부가텍스트
-const Description = ({ children, styleType = 'default' }: Pick<TextBoxProps, 'children' | 'styleType'>) => (
-  <div className={descriptionVar[styleType]}>{children}</div>
-);
+const Description = ({ children, styleType = 'default' }: Pick<TextBoxProps, 'children' | 'styleType'>) => {
+  const DEVICE_TYPE = useDevice();
+
+  return <div className={`${descriptionVar[styleType]} ${descriptionFontVar[DEVICE_TYPE]}`}>{children}</div>;
+};
 
 export default Description;
