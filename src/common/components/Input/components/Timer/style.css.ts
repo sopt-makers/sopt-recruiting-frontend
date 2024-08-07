f14dde6fbecea00f7df9797a77a4394ec50d6934
lydiacho <sheecho0804@ewhain.net>
2024-08-07 14:52:16 +0900

    design: Timer 반응형

--- src/common/components/Input/components/Timer/style.css.ts
+++ src/common/components/Input/components/Timer/style.css.ts
@@ -1,13 +1,36 @@
-import { style } from '@vanilla-extract/css';
+import { style, styleVariants } from '@vanilla-extract/css';
 
 import { theme } from 'styles/theme.css';
 
-export const timer = style({
+const timer = style({
   position: 'absolute',
-  right: 172,
   top: '50%',
   transform: 'translateY(-50%)',
   color: theme.color.lighterText,
-  ...theme.font.BODY_2_16_R,
+
   lineHeight: 'inherit',
+});
+
+export const timerVar = styleVariants({
+  DESK: [
+    timer,
+    {
+      right: 172,
+      ...theme.font.BODY_2_16_R,
+    },
+  ],
+  TAB: [
+    timer,
+    {
+      right: 137,
+      ...theme.font.BODY_2_16_R,
+    },
+  ],
+  MOB: [
+    timer,
+    {
+      right: 122,
+      ...theme.font.BODY_4_13_R,
+    },
+  ],
 });
