c167161aed2758e249c80f1a6b2728b6679078da
lydiacho <sheecho0804@ewhain.net>
2024-08-10 19:07:03 +0900

    design: 반응형 header active 페이지 bolder 추가

--- src/common/components/Layout/components/Header/Nav/MenuItem/style.css.ts
+++ src/common/components/Layout/components/Header/Nav/MenuItem/style.css.ts
@@ -50,6 +50,7 @@ export const menuLinkVar = styleVariants(
       },
       active: {
         color: theme.color.whiteButtonFill,
+        fontWeight: 'bolder',
       },
     },
     MOB: {
@@ -58,6 +59,7 @@ export const menuLinkVar = styleVariants(
       },
       active: {
         color: theme.color.whiteButtonFill,
+        fontWeight: 'bolder',
       },
     },
   },
