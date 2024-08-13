87d78ed748806b4f14ff5ce42294512139dd476f
Jeon Eonseok <jseokeon@gmail.com>
2024-08-14 02:01:46 +0900

    feat: result page 지원자 정보 마스킹 처리

--- src/views/ResultPage/components/FinalResult.tsx
+++ src/views/ResultPage/components/FinalResult.tsx
@@ -43,7 +43,7 @@ const Content = ({ pass }: { pass?: boolean }) => {
         <p className={contentVar[DEVICE_TYPE]}>
           <span>{`안녕하세요. ${SOPT_NAME}입니다.\n\n`}</span>
           <strong className={strongText[isMakers ? 'makers' : 'sopt']}>{`축하드립니다!`}</strong>
-          <span>
+          <span className="amp-block">
             {`
               ${name}님은 ${season}기 ${SOPT_NAME} ${!isMakers ? group : ''} 신입회원 모집에 최종 합격하셨습니다.
   
@@ -76,7 +76,7 @@ const Content = ({ pass }: { pass?: boolean }) => {
           </span>
         </p>
       ) : (
-        <p className={contentVar[DEVICE_TYPE]}>
+        <p className={`amp-block ${contentVar[DEVICE_TYPE]}`}>
           {`안녕하세요, ${SOPT_NAME}입니다.
           
           ${SOPT_NAME}에 관심을 갖고 지원해 주셔서 감사드립니다.
