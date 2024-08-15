4925d5eaa7d04170ad8feb1f40f3550a0783e02d
lydiacho <sheecho0804@ewhain.net>
2024-08-15 18:45:56 +0900

    design: ApplyHeader 반응형

--- src/views/ApplyPage/components/ApplyCategory/index.tsx
+++ src/views/ApplyPage/components/ApplyCategory/index.tsx
@@ -1,24 +1,29 @@
 import { memo } from 'react';
 import { Link } from 'react-router-dom';
 
+import { useDevice } from '@hooks/useDevice';
 import useScrollPosition from '@hooks/useScrollPosition';
 
 import { CATEGORY } from './constant';
-import { activeLinkStyle, categoryLinkStyle, categoryList, container } from './style.css';
+import { activeLinkStyleVar, categoryLinkStyleVar, categoryList, container, containerVar } from './style.css';
 
 interface ApplyCategoryProps {
   isReview: boolean;
   minIndex: number;
 }
 const ApplyCategory = memo(({ isReview, minIndex }: ApplyCategoryProps) => {
+  const deviceType = useDevice();
   const { isScrollingDown, isScrollTop } = useScrollPosition(isReview ? 380 : 950);
 
   return (
-    <nav className={container[isScrollingDown && !isScrollTop ? 'scrollDown' : 'scrollUp']}>
+    <nav
+      className={`${container[isScrollingDown && !isScrollTop ? 'scrollDown' : 'scrollUp']} ${containerVar[deviceType]}`}>
       <ul className={categoryList}>
         {CATEGORY.map(({ index, text, path }) => (
           <li key={path}>
-            <Link to={path} className={minIndex === index ? activeLinkStyle : categoryLinkStyle}>
+            <Link
+              to={path}
+              className={`${activeLinkStyleVar[minIndex === index ? 'active' : 'default']} ${categoryLinkStyleVar[deviceType]}`}>
               {text}
             </Link>
           </li>
