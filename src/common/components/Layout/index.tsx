82ed661f9b7b40f8f1694fb1ec1e89ee860d9081
lydiacho <sheecho0804@ewhain.net>
2024-08-16 21:26:18 +0900

    fix: 디렉토리명 카멜케이스로 수정

--- src/common/components/Layout/index.tsx
+++ src/common/components/Layout/index.tsx
@@ -2,8 +2,8 @@ import { PropsWithChildren } from 'react';
 import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
-import Head from './components/Settings/Head';
-import ScrollToTop from './components/Settings/ScrollToTop';
+import Head from './components/settings/Head';
+import ScrollToTop from './components/settings/ScrollToTop';
 import { container, mainWrapper } from './style.css';
 
 const Layout = ({ children }: PropsWithChildren) => {
