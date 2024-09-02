fbefd4aa490a295b63786cbf258b7db72493826d
Jeon Eonseok <jseokeon@gmail.com>
2024-09-02 21:29:49 +0900

    feat: custom render에 memory router 추가

--- src/tests/test-utils.tsx
+++ src/tests/test-utils.tsx
@@ -3,19 +3,33 @@ import DeviceTypeProvider from 'contexts/DeviceTypeProvider';
 import RecruitingInfoProvider from 'contexts/RecruitingInfoProvider';
 import ThemeProvider from 'contexts/ThemeProvider';
 import type { ReactElement, ReactNode } from 'react';
+import { MemoryRouter, type MemoryRouterProps } from 'react-router-dom';
 
-const AllTheProviders = ({ children }: { children: ReactNode }) => {
+interface AllTheProvidersProps extends MemoryRouterProps {
+  children: ReactNode;
+}
+
+const AllTheProviders = ({ children, ...memoryRouterProps }: AllTheProvidersProps) => {
   return (
-    <ThemeProvider>
-      <DeviceTypeProvider>
-        <RecruitingInfoProvider>{children}</RecruitingInfoProvider>
-      </DeviceTypeProvider>
-    </ThemeProvider>
+    <MemoryRouter {...memoryRouterProps}>
+      <ThemeProvider>
+        <DeviceTypeProvider>
+          <RecruitingInfoProvider>{children}</RecruitingInfoProvider>
+        </DeviceTypeProvider>
+      </ThemeProvider>
+    </MemoryRouter>
   );
 };
 
-const customRender = (ui: ReactElement, options?: Omit<RenderOptions, 'wrapper'>) =>
-  render(ui, { wrapper: AllTheProviders, ...options });
+const customRender = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>,
+  memoryRouterProps?: MemoryRouterProps,
+) =>
+  render(ui, {
+    wrapper: ({ children }) => <AllTheProviders {...memoryRouterProps}>{children}</AllTheProviders>,
+    ...options,
+  });
 
 export * from '@testing-library/react';
 export { customRender as render };
