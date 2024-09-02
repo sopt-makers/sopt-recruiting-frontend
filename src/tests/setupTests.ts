cbee78618551d782db5c5560fc8058fb85ae636f
Jeon Eonseok <jseokeon@gmail.com>
2024-09-02 22:35:59 +0900

    fix: fillStyle을 null로 설정할 수 없는 에러 해결

--- src/tests/setupTests.ts
+++ src/tests/setupTests.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom/vitest';
+
+/* @ts-ignore */
+HTMLCanvasElement.prototype.getContext = () => {
+  return {
+    fillStyle: '',
+    fillRect: vitest.fn(),
+  };
+};
