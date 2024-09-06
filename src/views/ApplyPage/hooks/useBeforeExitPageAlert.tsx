c099e36742c9e56797c13d8348a9ccfae0d1e567
Jeon Eonseok <jseokeon@gmail.com>
2024-09-06 22:13:49 +0900

    refactor: useEventListener 적용

--- src/views/ApplyPage/hooks/useBeforeExitPageAlert.tsx
+++ src/views/ApplyPage/hooks/useBeforeExitPageAlert.tsx
@@ -1,17 +1,13 @@
-import { useEffect } from 'react';
+import useEventListener from '@hooks/useEventListener';
 
 /** 페이지 이탈 시 alert 띄우기 */
 const useBeforeExitPageAlert = () => {
-  useEffect(() => {
-    const handleBeforeUnload = (e: BeforeUnloadEvent) => {
-      e.preventDefault();
-      e.returnValue = ''; // Included for legacy support, e.g. Chrome/Edeg < 119;
-    };
+  const handleBeforeUnload = (e: BeforeUnloadEvent) => {
+    e.preventDefault();
+    e.returnValue = ''; // Included for legacy support, e.g. Chrome/Edeg < 119;
+  };
 
-    window.addEventListener('beforeunload', handleBeforeUnload);
-
-    return () => window.removeEventListener('beforeunload', handleBeforeUnload);
-  }, []);
+  useEventListener('beforeunload', handleBeforeUnload);
 };
 
 export default useBeforeExitPageAlert;
