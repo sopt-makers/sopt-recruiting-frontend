61ecc98dcbe2fff0163f2818b3b51985d36356cf
Jeon Eonseok <jseokeon@gmail.com>
2024-09-06 21:31:20 +0900

    feat: 기존 axios instace -> fetch instance로 대체

--- src/common/apis/instance.ts
+++ src/common/apis/instance.ts
@@ -1,10 +1,23 @@
-import axios from 'axios';
+const baseURL = import.meta.env.VITE_BASE_URL;
 
-const instance = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL,
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
+interface FetchOptions extends RequestInit {
+  headers?: Record<string, string>;
+}
+
+const instance = async (url: string, options: FetchOptions = {}) => {
+  const response = await fetch(`${baseURL}${url}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      ...options.headers,
+    },
+    ...options,
+  });
+
+  if (!response.ok) {
+    throw new Error('network response가 도착하지 않았어요');
+  }
+
+  return response.json();
+};
 
 export default instance;
