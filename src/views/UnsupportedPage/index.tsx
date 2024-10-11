a56c2ba9ec1715599668e4a84332a42fe30ff3ec
Eonseok Jeon <121864459+eonseok-jeon@users.noreply.github.com>
2024-10-11 14:06:25 +0900

    [Design] 지원 안 되는 브라우저 안내 페이지 퍼블리싱 (#464)
    * design: 지원되지 않는 페이지 퍼블리싱
    
    * fix: icon들 png -> svg 변경
    
    * desing: 반응형 구현
    
    * design: 중앙 정렬
    
    * fix: 문의하기 링크 수정
    
    * design: 높이 수정
    
    * design: header 추가
    
    * design: layout 추가
    
    * design: css 수정
    
    * design: sopt logo 변경
    
    * fix: 비교 연산자 수정

--- src/views/UnsupportedPage/index.tsx
+++ src/views/UnsupportedPage/index.tsx
@@ -1,40 +1,102 @@
+import Callout from '@components/Callout';
+import {
+  browserButtonVar,
+  browserIconVar,
+  browsersWrapper,
+  contactButtonVar,
+  wrapper,
+  headingVar,
+  infoText,
+  infoTitle,
+  instructionVar,
+  updateInfoTitle,
+  updateInfoWrapperVar,
+} from './style.css';
+import ChromeIcon from './assets/ChromeIcon';
+import EdgeIcon from './assets/EdgeIcon';
+import FirefoxIcon from './assets/FirefoxIcon';
+import SafariIcon from './assets/SafariIcon';
+import MakersLogo from '@assets/MakersLogo';
+import { useDeviceType } from 'contexts/DeviceTypeProvider';
+import { containerSizeVer, containerVar, logoVar } from '@components/Layout/components/Header/style.css';
+import { container, mainWrapper } from '@components/Layout/style.css';
+import SoptLogo from '@assets/SoptLogo';
+
 const UnsupportedPage = () => {
+  const { deviceType } = useDeviceType();
+  const isMakers = import.meta.env.MODE === 'makers';
+  const logoVariant = logoVar[deviceType];
+
   return (
-    <section>
-      <article>
-        <p>지원되지 않는 브라우저예요</p>
-        <p>지원 안되는 브라우저 목록들</p>
-        <ol>
-          <li>1. 서비스가 종료된 브라우저</li>
-          <li>2. 업데이트된지 2.5년이 지난 브라우저</li>
-          <li>3. 전 세계 0.3% 미만의 유저를 보유한 브라우저</li>
-        </ol>
-        <br />
-        <p>업데이트 하러 가기</p>
-        <a href="https://www.google.com/chrome/update/" target="_blank" rel="noreferrer noopener">
-          Chrome
-        </a>
-        <a href="https://support.apple.com/en-us/102665" target="_blank" rel="noreferrer noopener">
-          Safari
-        </a>
-        <a
-          href="https://support.microsoft.com/en-au/topic/microsoft-edge-update-settings-af8aaca2-1b69-4870-94fe-18822dbb7ef1"
-          target="_blank"
-          rel="noreferrer noopener">
-          Edge
-        </a>
-        <a
-          href="https://support.mozilla.org/en-US/kb/update-firefox-latest-release"
-          target="_blank"
-          rel="noreferrer noopener">
-          Firefox
-        </a>
-      </article>
-      <p>{`문의사항이 있다면\n아래 ‘문의하기’를 이용해 주세요`}</p>
-      <a id="chat-channel-button" href="https://pf.kakao.com/_JdTKd" target="_blank" rel="noreferrer noopener">
-        문의하기
-      </a>
-    </section>
+    <div className={container}>
+      <header className={`${containerVar['default']} ${containerSizeVer[deviceType]}`}>
+        {isMakers ? <MakersLogo className={logoVariant} /> : <SoptLogo className={logoVariant} />}
+      </header>
+      <main className={mainWrapper}>
+        <section className={wrapper}>
+          <h1 className={headingVar[deviceType]}>지원되지 않는 브라우저예요</h1>
+          <Callout>
+            <div>
+              <p className={infoTitle}>지원하지 않는 브라우저 목록이에요.</p>
+              <ol className={infoText}>
+                <li>1. 서비스가 종료된 브라우저</li>
+                <li>2. 업데이트 후 2.5년이 지난 브라우저</li>
+                <li>3. 전 세계 0.3% 미만의 점유율을 보유한 브라우저</li>
+              </ol>
+            </div>
+          </Callout>
+          <article className={updateInfoWrapperVar[deviceType]}>
+            <p className={updateInfoTitle}>브라우저 업데이트 하기</p>
+            <div className={browsersWrapper}>
+              <a
+                className={browserButtonVar[deviceType]}
+                href="https://www.google.com/chrome/update/"
+                target="_blank"
+                rel="noreferrer noopener">
+                <ChromeIcon className={browserIconVar[deviceType]} />
+                <span>Chrome</span>
+              </a>
+              <a
+                className={browserButtonVar[deviceType]}
+                href="https://support.apple.com/en-us/102665"
+                target="_blank"
+                rel="noreferrer noopener">
+                <SafariIcon className={browserIconVar[deviceType]} />
+                <span>Safari</span>
+              </a>
+              <a
+                className={browserButtonVar[deviceType]}
+                href="https://support.microsoft.com/en-au/topic/microsoft-edge-update-settings-af8aaca2-1b69-4870-94fe-18822dbb7ef1"
+                target="_blank"
+                rel="noreferrer noopener">
+                <EdgeIcon className={browserIconVar[deviceType]} />
+                <span>Edge</span>
+              </a>
+              <a
+                className={browserButtonVar[deviceType]}
+                href="https://support.mozilla.org/en-US/kb/update-firefox-latest-release"
+                target="_blank"
+                rel="noreferrer noopener">
+                <FirefoxIcon className={browserIconVar[deviceType]} />
+                <span>Firefox</span>
+              </a>
+            </div>
+          </article>
+          <p
+            className={
+              instructionVar[deviceType]
+            }>{`문제가 지속적으로 발생하거나 문의사항이 있다면\n아래 ‘문의하기’를 이용해 주세요`}</p>
+          <a
+            id="chat-channel-button"
+            href={isMakers ? 'https://pf.kakao.com/_sxaIWG' : 'https://pf.kakao.com/_JdTKd'}
+            target="_blank"
+            rel="noreferrer noopener"
+            className={contactButtonVar[deviceType]}>
+            문의하기
+          </a>
+        </section>
+      </main>
+    </div>
   );
 };
 
