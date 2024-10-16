7374bdbf3ae09e8a43f78a29287011af81674b87
lydiacho <sheecho0804@ewhain.net>
2024-10-17 01:38:29 +0900

    feat: string -> Date 형변환 처리 추가

--- src/common/utils/dateFormatter.ts
+++ src/common/utils/dateFormatter.ts
@@ -1,22 +1,30 @@
-export const _isBefore = (date: Date, dateToCompare: Date): boolean => {
+export const _isBefore = (date: Date | string, dateToCompare: Date | string): boolean => {
+  if (typeof date === 'string') date = new Date(date);
+  if (typeof dateToCompare === 'string') dateToCompare = new Date(dateToCompare);
   return date.getTime() < dateToCompare.getTime();
 };
 
-export const _isAfter = (date: Date, dateToCompare: Date): boolean => {
+export const _isAfter = (date: Date | string, dateToCompare: Date | string): boolean => {
+  if (typeof date === 'string') date = new Date(date);
+  if (typeof dateToCompare === 'string') dateToCompare = new Date(dateToCompare);
   return date.getTime() > dateToCompare.getTime();
 };
 
-export const _differenceInSeconds = (laterDate: Date, earlierDate: Date): number => {
+export const _differenceInSeconds = (laterDate: Date | string, earlierDate: Date | string): number => {
+  if (typeof laterDate === 'string') laterDate = new Date(laterDate);
+  if (typeof earlierDate === 'string') earlierDate = new Date(earlierDate);
   return Math.floor((laterDate.getTime() - earlierDate.getTime()) / 1000);
 };
 
-export const _subMinutes = (date: Date, amount: number): Date => {
+export const _subMinutes = (date: Date | string, amount: number): Date => {
+  if (typeof date === 'string') date = new Date(date);
   const newDate = new Date();
   newDate.setTime(date.getTime() - amount * 60 * 1000);
   return newDate;
 };
 
-export const _format = (date: Date, formatStr: string): string => {
+export const _format = (date: Date | string, formatStr: string): string => {
+  if (typeof date === 'string') date = new Date(date);
   const days = ['월', '화', '수', '목', '금', '토', '일'];
   const formatter: { [key: string]: string } = {
     M: (date.getMonth() + 1).toString(),
--- src/common/utils/dateTest.ts
+++ src/common/utils/dateTest.ts
@@ -9,26 +9,24 @@ import { _differenceInSeconds, _format, _isAfter, _isBefore, _subMinutes } from 
 const dateTest = () => {
   const sampleDate = '2024-06-20T15:27:45.000Z';
 
-  const isBefore라브 = isBefore(new Date(1989, 6, 10), new Date(1987, 1, 11));
-  const isBefore유틸 = _isBefore(new Date(1989, 6, 10), new Date(1987, 1, 11));
+  const isBefore라브 = isBefore(sampleDate, new Date(1987, 1, 11));
+  const isBefore유틸 = _isBefore(sampleDate, new Date(1987, 1, 11));
   console.log('🚀isBefore Test:', isBefore라브 === isBefore유틸);
 
-  const isAfter라브 = isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11));
-  const isAfter유틸 = _isAfter(new Date(1989, 6, 10), new Date(1987, 1, 11));
+  const isAfter라브 = isAfter(sampleDate, new Date(1987, 1, 11));
+  const isAfter유틸 = _isAfter(sampleDate, new Date(1987, 1, 11));
   console.log('🚀isAfter Test:', isAfter라브 === isAfter유틸);
 
-  const dIS라브 = differenceInSeconds(new Date(2014, 6, 2, 12, 30, 20, 700), new Date(2014, 6, 2, 12, 30, 7, 999));
-  const dIS유틸 = _differenceInSeconds(new Date(2014, 6, 2, 12, 30, 20, 700), new Date(2014, 6, 2, 12, 30, 7, 999));
+  const dIS라브 = differenceInSeconds(sampleDate, new Date(2014, 6, 2, 12, 30, 7, 999));
+  const dIS유틸 = _differenceInSeconds(sampleDate, new Date(2014, 6, 2, 12, 30, 7, 999));
   console.log('🚀differenceInSeconds Test:', dIS라브 === dIS유틸);
 
-  const subMinutes라브 = subMinutes(new Date(2014, 6, 10, 12, 0), 30);
-  const subMinutes유틸 = _subMinutes(new Date(2014, 6, 10, 12, 0), 30);
+  const subMinutes라브 = subMinutes(new Date(sampleDate), 30);
+  const subMinutes유틸 = _subMinutes(new Date(sampleDate), 30);
   console.log('🚀subMinutes Test:', subMinutes라브.getTime() === subMinutes유틸.getTime());
 
-  const format라브 = format(new Date(sampleDate), 'M월 dd일 (E) aaa HH시 mm분', { locale: ko });
-  const format유틸 = _format(new Date(sampleDate), 'M월 dd일 (E) aaa HH시 mm분');
-  console.log('🚀format by lib: ', format라브);
-  console.log('🚀format by util: ', format유틸);
+  const format라브 = format(sampleDate, 'M월 dd일 (E) aaa HH시 mm분', { locale: ko });
+  const format유틸 = _format(sampleDate, 'M월 dd일 (E) aaa HH시 mm분');
   console.log('🚀format Test:', format유틸 === format라브);
 };
 
