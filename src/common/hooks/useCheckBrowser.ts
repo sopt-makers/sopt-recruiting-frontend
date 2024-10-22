9db6557de491fd2cdf9fa74a3057d428fae479e1
Jeon Eonseok <jseokeon@gmail.com>
2024-10-22 10:26:03 +0900

    fix: chrome 판단 regex 수정

--- src/common/hooks/useCheckBrowser.ts
+++ src/common/hooks/useCheckBrowser.ts
@@ -2,7 +2,18 @@ import { useEffect } from 'react';
 
 const useCheckBrowser = () => {
   useEffect(() => {
-    const isChrome = /Chrome/i.test(window.navigator.userAgent);
+    const userAgent = window.navigator.userAgent;
+
+    const chromeRegexes = [
+      /\b(?:crmo|crios)\/([\w\.]+)/i, // Chrome for Android/iOS
+      /headlesschrome(?:\/([\w\.]+)| )/i, // Chrome Headless
+      / wv\).+(chrome)\/([\w\.]+)/i, // Chrome WebView
+      /chrome\/([\w\.]+) mobile/i, // Chrome Mobile
+      /(cros) [\w]+(?:\)| ([\w\.]+)\b)/i, // ChromeOS (Chromium OS)
+    ];
+
+    const isChrome = chromeRegexes.some((regex) => regex.test(userAgent));
+
     if (!isChrome) {
       alert('원활한 지원을 위해 크롬(Chrome) 브라우저 사용을 권장드려요.');
     }
