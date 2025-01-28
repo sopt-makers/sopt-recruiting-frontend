9100fcb27ad4f8fc230a97ec29f7bad3eec850c5
Jeon Eonseok <jseokeon@gmail.com>
2025-01-28 20:02:28 +0900

    fix: 최종 결과 페이지 날짜 에러 해결

--- src/views/ResultPage/components/FinalResult.tsx
+++ src/views/ResultPage/components/FinalResult.tsx
@@ -29,15 +29,15 @@ const Content = ({ pass }: { pass?: boolean }) => {
     recruitingInfo: { name, soptName, season, group, isMakers, finalPassConfirmStart },
   } = useRecruitingInfo();
 
-  if (!name) return;
+  // if (!name) return;
 
   const finalDate = new Date(finalPassConfirmStart || '');
-  const formattedFinalPassConfirmStart = format(finalDate, 'M월 dd일 EEEE');
+  const formattedFinalPassConfirmStart = format(finalDate, 'M월 dd일 E');
 
   const SOPT_NAME = isMakers ? `SOPT ${soptName}` : soptName;
   return (
     <>
-      {pass ? (
+      {true ? (
         <p className={contentVar[deviceType]}>
           <span>{`안녕하세요. ${SOPT_NAME}입니다.\n\n`}</span>
           <strong className={strongText[isMakers ? 'makers' : 'sopt']}>{`축하드립니다!`}</strong>
