1b5fa3d0d998a94b7a8a0bf829304f21ae6d619b
namdaeun <nde40345@gmail.com>
2026-02-06 13:53:28 +0900

    hotfix: 모집 공고 링크 변경

--- src/common/components/Layout/components/Header/contants.ts
+++ src/common/components/Layout/components/Header/contants.ts
@@ -23,7 +23,7 @@ export const MENU_ITEMS: menuItemsTypes[] = [
 export const MENU_ITEMS_MAKERS: menuItemsTypes[] = [
   {
     text: '모집공고',
-    path: 'https://sopt-makers.notion.site/37-22976042aac28009811df7026480fa54',
+    path: 'https://sopt-makers.notion.site/38-2fd76042aac280f39548e46b98e2a06a?pvs=73',
     amplitudeId: 'click-gnb-recruitment_notice_makers',
     target: '_blank',
   },
