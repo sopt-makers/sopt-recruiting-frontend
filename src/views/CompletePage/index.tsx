df905aee85cf186305d5d80f029a0d13f01c7040
namdaeun <nde40345@gmail.com>
2026-03-06 23:23:04 +0900

    hotfix: 이메일 관련 문구 제거

--- src/views/CompletePage/index.tsx
+++ src/views/CompletePage/index.tsx
@@ -1,11 +1,10 @@
 import Button from '@components/Button';
-import Callout from '@components/Callout';
 import { useDeviceType } from 'contexts/DeviceTypeProvider';
 import { useRecruitingInfo } from 'contexts/RecruitingInfoProvider';
 
 import Survey from './components/Survey';
 import IconCheckmark from './icons/IconCheckmark';
-import { container, iconVar, mainTextVar, subTextVar } from './style.css';
+import { container, iconVar, mainTextVar } from './style.css';
 
 const CompletePage = () => {
   const { deviceType } = useDeviceType();
@@ -26,11 +25,7 @@ const CompletePage = () => {
         className={
           mainTextVar[deviceType]
         }>{`${name}님의\n${season}기 ${__IS_MAKERS__ ? soptName : group} 지원서가 접수되었습니다.`}</p>
-      <p className={subTextVar[deviceType]}>이메일로 지원 접수 완료 알림이 발송되었습니다.</p>
-      <Callout
-        style={{
-          marginBottom: 35,
-        }}>{`이메일 도착 시점에 차이가 있을 수 있습니다.\n이메일이 오지 않으면 스팸 메일함을 확인해주세요.`}</Callout>
+
       <Button eventName="click-complete-my" onClick={handleClickMyPage}>
         마이페이지로 이동하기
       </Button>
