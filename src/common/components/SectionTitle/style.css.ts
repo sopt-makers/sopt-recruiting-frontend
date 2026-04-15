4c541475597e95a1811adda0132661676698d4e5
namdaeun <nde40345@gmail.com>
2026-04-15 20:25:03 +0900

    feat: add SectionTitle component

---
+++ src/common/components/SectionTitle/style.css.ts
@@ -0,0 +1,46 @@
+import { style, styleVariants } from '@vanilla-extract/css';
+import { theme } from 'styles/theme.css';
+
+export const wrapper = style({
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'center',
+  justifyContent: 'center',
+});
+
+export const label = style({
+  color: theme.color.primary,
+});
+
+export const labelVar = styleVariants({
+  DESK: [
+    label,
+    {
+      ...theme.font.BODY_2_16_M,
+    },
+  ],
+  TAB: [
+    label,
+    {
+      ...theme.font.BODY_2_16_M,
+    },
+  ],
+  MOB: [
+    label,
+    {
+      ...theme.font.LABEL_4_12_SB,
+    },
+  ],
+});
+
+export const titleVar = styleVariants({
+  DESK: {
+    ...theme.font.HEADING_2_32_B,
+  },
+  TAB: {
+    ...theme.font.HEADING_2_32_B,
+  },
+  MOB: {
+    ...theme.font.HEADING_6_18_B,
+  },
+});
