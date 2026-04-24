9f3dbd4e9b957fc827ad5a88a691ec91c5c2536d
namdaeun <nde40345@gmail.com>
2026-04-24 17:31:17 +0900

    style: page wrapper @media처리

--- src/views/IntroducePage/index.tsx
+++ src/views/IntroducePage/index.tsx
@@ -2,16 +2,15 @@ import CoreValue from 'views/IntroducePage/components/CoreValue';
 import FAQ from 'views/IntroducePage/components/FAQ';
 import RecruitmentTarget from 'views/IntroducePage/components/RecruitmentTarget';
 import { wrapper } from './style.css';
-import { useDeviceType } from 'contexts/DeviceTypeProvider';
 import Schedule from 'views/IntroducePage/components/Schedule';
 import Inquiry from 'views/IntroducePage/components/Inquiry';
 import SoptPart from 'views/IntroducePage/components/SoptPart';
+import ApplyHeader from 'views/IntroducePage/components/ApplyHeader';
 
 const IntroducePage = () => {
-  const { deviceType } = useDeviceType();
-
   return (
-    <div className={wrapper[deviceType]}>
+    <div className={wrapper}>
+      <ApplyHeader />
       <RecruitmentTarget />
       <SoptPart />
       <CoreValue />
