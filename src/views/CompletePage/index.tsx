8e517347aade7030dad87e485d3166ff2760e122
constantly-dev <rntqhdl10@naver.com>
2026-04-25 14:56:47 +0900

    feat: 제출 완료 페이지 callout 추가 및 라이팅 수정

--- src/views/CompletePage/index.tsx
+++ src/views/CompletePage/index.tsx
@@ -4,7 +4,8 @@ import { useRecruitingInfo } from 'contexts/RecruitingInfoProvider';
 
 import Survey from './components/Survey';
 import IconCheckmark from './icons/IconCheckmark';
-import { container, iconVar, mainTextVar } from './style.css';
+import { container, iconVar, mainTextVar, subTextVar, buttonWrapperVar } from './style.css';
+import Callout from '@components/Callout';
 
 const CompletePage = () => {
   const { deviceType } = useDeviceType();
@@ -25,10 +26,20 @@ const CompletePage = () => {
         className={
           mainTextVar[deviceType]
         }>{`${name}님의\n${season}기 ${__IS_MAKERS__ ? soptName : group} 지원서가 접수되었습니다.`}</p>
+      <p className={subTextVar[deviceType]}>
+        {/* TODO: 서류 결과일 변수 변경 */}
+        {`이메일로 지원 접수 완료 알림이 발송되었습니다.\n
+        서류 결과는 2026/03/18에 발표됩니다.`}
+      </p>
 
-      <Button eventName="click-complete-my" onClick={handleClickMyPage}>
-        마이페이지로 이동하기
-      </Button>
+      <Callout>
+        메일 도착 시점이 다를 수 있으니, 메일이 확인되지 않는 경우 스팸 메일함을 확인해 주시기 바랍니다.
+      </Callout>
+      <div className={buttonWrapperVar[deviceType]}>
+        <Button eventName="click-complete-my" onClick={handleClickMyPage}>
+          마이페이지로 이동하기
+        </Button>
+      </div>
       <Survey />
     </section>
   );
