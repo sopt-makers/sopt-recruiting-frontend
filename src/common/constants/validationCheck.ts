8392efdffcbba229213349c2dd98bbc13c215ad6
namdaeun <nde40345@gmail.com>
2026-04-27 14:13:52 +0900

    chore: 라이팅 수정

--- src/common/constants/validationCheck.ts
+++ src/common/constants/validationCheck.ts
@@ -18,7 +18,7 @@ export const VALIDATION_CHECK = {
     errorText: '잘못된 이메일 형식이에요. 이메일을 정확하게 입력해주세요.',
     errorTextExistence: '이미 존재하는 계정이에요.',
     errorTextNonexistence: '존재하지 않은 계정이에요.',
-    notMatchErrorText: '잘못된 이메일 혹은 비밀번호예요.',
+    notMatchErrorText: '이메일 또는 비밀번호가 잘못되었습니다. 다시 확인해 주세요.',
   },
   emailConfirm: {
     errorText: '이메일이 일치하지 않아요.',
@@ -32,7 +32,7 @@ export const VALIDATION_CHECK = {
     pattern: /^[a-zA-Z0-9!@#$%^&*()_+[\]{};':="\\|,.<>/?`~-]{4,}$/,
     maxLength: 100,
     errorText: '비밀번호는 4자리 이상으로 구성 해주세요.',
-    notMatchErrorText: '잘못된 이메일 혹은 비밀번호예요.',
+    notMatchErrorText: '이메일 또는 비밀번호가 잘못되었습니다. 다시 확인해 주세요.',
   },
   passwordConfirm: {
     maxLength: 100,
--- src/views/SignInPage/components/SignInInfo/index.tsx
+++ src/views/SignInPage/components/SignInInfo/index.tsx
@@ -2,36 +2,34 @@ import { Link } from 'react-router-dom';
 
 import Callout from '@components/Callout';
 import Title from '@components/Title';
-import { useDeviceType } from 'contexts/DeviceTypeProvider';
 import { useRecruitingInfo } from 'contexts/RecruitingInfoProvider';
 
-import { calloutButtonVar } from './style.css';
+import { infoContainerVar, calloutButtonVar, infoTextVar } from './style.css';
 import AmplitudeEventTrack from '@components/Button/AmplitudeEventTrack';
 import { IconChevronRight } from '@sopt-makers/icons';
 
 const SignInInfo = () => {
-  const { deviceType } = useDeviceType();
   const {
-    recruitingInfo: { soptName, season, group },
+    recruitingInfo: { season },
   } = useRecruitingInfo();
 
   return (
-    <>
+    <div className={infoContainerVar}>
       <Title>로그인</Title>
       <Callout
         Button={
           <AmplitudeEventTrack eventName="click-signin-signup">
-            <Link to="/sign-up" className={calloutButtonVar[deviceType]}>
+            <Link to="/sign-up" className={calloutButtonVar}>
               회원가입
               <IconChevronRight style={{ width: 20, height: 20 }} />
             </Link>
           </AmplitudeEventTrack>
         }>
-        <p>
-          {`${season}기 지원이 처음이라면 '회원가입'을 진행해 주시기 바랍니다. 이전 기수에 가입한 경험이 있더라도, 기수별로 새로 회원가입을 해야 합니다.`}
+        <p className={infoTextVar}>
+          {`${season}기 지원이 처음이라면 '회원가입'을 진행해 주세요. 이전 기수에 가입한 경험이 있더라도, 기수별로 새로 회원가입을 해야 합니다.`}
         </p>
       </Callout>
-    </>
+    </div>
   );
 };
 
