18f253bb93a559b2113f31d17bb5d12d51985f78
namdaeun <nde40345@gmail.com>
2026-04-27 15:44:29 +0900

    chore: import 수정

--- src/views/IntroducePage/index.tsx
+++ src/views/IntroducePage/index.tsx
@@ -5,16 +5,16 @@ import { wrapper } from './style.css';
 import Schedule from 'views/IntroducePage/components/Schedule';
 import Inquiry from 'views/IntroducePage/components/Inquiry';
 import SoptPart from 'views/IntroducePage/components/SoptPart';
-import ApplyHeader from 'views/IntroducePage/components/ApplySection';
 import useDate from '@hooks/useDate';
 import BottomApplyCTA from 'views/IntroducePage/components/BottomApplyCTA';
+import ApplySection from 'views/IntroducePage/components/ApplySection';
 
 const IntroducePage = () => {
   const { NoMoreRecruit, NoMoreApply } = useDate();
 
   return (
     <div className={wrapper}>
-      <ApplyHeader />
+      <ApplySection />
       <RecruitmentTarget />
       <SoptPart />
       <CoreValue />
