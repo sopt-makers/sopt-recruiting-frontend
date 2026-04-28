b939e4d6b72c3083d143e4608351b2099bd1a1c1
namdaeun <nde40345@gmail.com>
2026-04-28 15:06:09 +0900

    feat: update Schedule component to use useDate

--- src/views/IntroducePage/components/Schedule/index.tsx
+++ src/views/IntroducePage/components/Schedule/index.tsx
@@ -1,14 +1,8 @@
 import SectionTitle from '@components/SectionTitle';
-import { DUMMY_SCHEDULE, TITLE } from 'views/IntroducePage/constants/constant';
-import {
-  gridWrapper,
-  oddText,
-  evenText,
-  highlight,
-  container,
-  scheduleGroup,
-  wrapper,
-} from './style.css';
+import { TITLE } from 'views/IntroducePage/constants/constant';
+import { gridWrapper, oddText, evenText, highlight, container, scheduleGroup, wrapper } from './style.css';
+import useDate from '@hooks/useDate';
+import { format } from '@utils/dateFormatter';
 
 const Schedule = () => {
   return (
@@ -22,15 +16,15 @@ const Schedule = () => {
 export default Schedule;
 
 const ScheduleBox = () => {
-  const {
-    group,
-    applicationStartTime,
-    applicationEndTime,
-    interviewStartTime,
-    interviewEndTime,
-    finalResultTime,
-    orientationTime,
-  } = DUMMY_SCHEDULE;
+  const { group, applicationStart, applicationEnd, interviewStart, interviewEnd, finalResultStart } = useDate();
+
+  const formattedApplicationStart = applicationStart ? format(applicationStart, 'M월 dd일 aaa hh시') : '';
+  const formattedApplicationStartHour = applicationStart ? format(applicationStart, 'aaa hh시') : '';
+  const formattedApplicationEnd = applicationEnd ? format(applicationEnd, 'M월 dd일 aaa hh시') : '';
+  const formattedApplicationEndHour = applicationEnd ? format(applicationEnd, 'aaa hh시') : '';
+  const formattedInterviewStart = interviewStart ? format(interviewStart, 'M월 dd일') : '';
+  const formattedInterviewEnd = interviewEnd ? format(interviewEnd, 'M월 dd일') : '';
+  const formattedFinalResultStart = finalResultStart ? format(finalResultStart, 'M월 dd일') : '';
 
   return (
     <div className={container}>
@@ -38,26 +32,27 @@ const ScheduleBox = () => {
         <div className={scheduleGroup}>
           <p className={oddText}>{group} 서류 접수</p>
           <div className={evenText}>
-            {`${applicationStartTime.date} `}
-            <span className={highlight}>{applicationStartTime.hour}</span>
-            {` ~ ${applicationEndTime.date} `}
-            <span className={highlight}>{applicationEndTime.hour}</span>
+            {`${formattedApplicationStart} `}
+            <span className={highlight}>{formattedApplicationStartHour}</span>
+            {` ~ ${formattedApplicationEnd} `}
+            <span className={highlight}>{formattedApplicationEndHour}</span>
           </div>
         </div>
 
         <div className={scheduleGroup}>
           <p className={oddText}>{group} 면접</p>
-          <div className={evenText}>{`${interviewStartTime.date} ~ ${interviewEndTime.date}`}</div>
+          <div className={evenText}>{`${formattedInterviewStart} ~ ${formattedInterviewEnd}`}</div>
         </div>
 
         <div className={scheduleGroup}>
           <p className={oddText}>{group} 최종 결과 발표</p>
-          <div className={evenText}>{`${finalResultTime.date} `}</div>
+          <div className={evenText}>{formattedFinalResultStart}</div>
         </div>
 
+        {/* TODO: 오리엔테이션 날짜 어드민에서 가져오기 */}
         <div className={scheduleGroup}>
           <p className={oddText}>오리엔테이션</p>
-          <div className={evenText}>{`${orientationTime.date} `}</div>
+          <div className={evenText}>{'3월 27일'}</div>
         </div>
       </div>
     </div>
