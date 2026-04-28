cb640dc121a9ab3581754e9cf92107438b9af8bc
constantly-dev <rntqhdl10@naver.com>
2026-04-28 23:54:05 +0900

    feat: 완료 페이지 서류 결과 날짜 format 추가

--- src/views/CompletePage/index.tsx
+++ src/views/CompletePage/index.tsx
@@ -1,6 +1,7 @@
 import Button from '@components/Button';
 import { useDeviceType } from 'contexts/DeviceTypeProvider';
 import { useRecruitingInfo } from 'contexts/RecruitingInfoProvider';
+import { format } from '@utils/dateFormatter';
 
 import Survey from './components/Survey';
 import IconCheckmark from './icons/IconCheckmark';
@@ -10,7 +11,7 @@ import Callout from '@components/Callout';
 const CompletePage = () => {
   const { deviceType } = useDeviceType();
   const {
-    recruitingInfo: { name, season, group, soptName },
+    recruitingInfo: { name, season, group, soptName, applicationResultStart },
   } = useRecruitingInfo();
 
   const handleClickMyPage = () => {
@@ -27,9 +28,8 @@ const CompletePage = () => {
           mainTextVar[deviceType]
         }>{`${name}님의\n${season}기 ${__IS_MAKERS__ ? soptName : group} 지원서가 접수되었습니다.`}</p>
       <p className={subTextVar[deviceType]}>
-        {/* TODO: 서류 결과일 변수 변경 */}
         {`이메일로 지원 접수 완료 알림이 발송되었습니다.\n
-        서류 결과는 2026/03/18에 발표됩니다.`}
+        서류 결과는 ${applicationResultStart ? format(applicationResultStart, 'yyyy/MM/dd') : ''}에 발표됩니다.`}
       </p>
 
       <Callout>
