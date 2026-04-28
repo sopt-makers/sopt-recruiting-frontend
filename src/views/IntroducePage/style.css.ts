be09e1d9762d35066e63788a6bfb7235a669c26c
namdaeun <nde40345@gmail.com>
2026-04-28 17:09:49 +0900

    feat: divider 추가

--- src/views/IntroducePage/style.css.ts
+++ src/views/IntroducePage/style.css.ts
@@ -6,13 +6,20 @@ export const wrapper = style({
   display: 'flex',
   width: '100%',
   flexDirection: 'column',
-  padding: '0 40px',
+  padding: '0 40px 80px 40px',
   backgroundColor: colors.gray10,
   scrollBehavior: 'smooth',
 
   '@media': {
-    [breakpoints.mobile]: { padding: '0 20px' },
+    [breakpoints.mobile]: { padding: '0 20px 40px 20px' },
   },
+});
+
+export const divider = style({
+  width: '100%',
+  height: '8px',
+  // TODO: colors.gray20 적용 필요
+  backgroundColor: '#F6F6F6',
 });
 
 globalStyle(`${wrapper} > * + *`, {
