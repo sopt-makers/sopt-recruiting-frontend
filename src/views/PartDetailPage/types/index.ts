c186917c80a97d25cf68da87cd79249dd66d4372
constantly-dev <rntqhdl10@naver.com>
2026-04-29 21:50:45 +0900

    feat: 활동 후기 API 연동 - 타입, API 함수, 훅 추가

--- src/views/PartDetailPage/types/index.ts
+++ src/views/PartDetailPage/types/index.ts
@@ -25,3 +25,28 @@ export type PartDetailResponse = {
   preferences: string[];
   partCurriculum: string[];
 };
+
+export type ReviewApiItem = {
+  id: number;
+  title: string;
+  author: string;
+  authorProfileImageUrl: string;
+  generation: number;
+  description: string;
+  partType: string;
+  category: string;
+  subject: string[];
+  thumbnailUrl: string;
+  platform: string;
+  url: string;
+};
+
+export type ReviewsResponse = {
+  limit: number;
+  totalCount: number;
+  totalPage: number;
+  currentPage: number;
+  data: ReviewApiItem[];
+  hasNextPage: boolean;
+  hasPrevPage: boolean;
+};
