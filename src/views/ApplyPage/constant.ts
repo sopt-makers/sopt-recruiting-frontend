4a916b9d7eaa811e1348dd2d1c45e39541c67b04
constantly-dev <rntqhdl10@naver.com>
2026-07-31 03:50:09 +0700

    fix: makers 시즌 데이터 업데이트 및 모집공고 링크 공용 상수 분리
    OT_DATE_TEXT_MAKERS를 8월 22일(토)로 갱신하고, GNB 모집공고 링크를
    39기 공지 notion 페이지로 업데이트. 이 링크가 다른 곳에서도 재사용될
    수 있도록 src/common/constants/links.ts로 분리.

--- src/views/ApplyPage/constant.ts
+++ src/views/ApplyPage/constant.ts
@@ -1,5 +1,5 @@
 // makers는 어드민 연동이 안 돼있어 ot 날짜 하드코딩
-export const OT_DATE_TEXT_MAKERS = '2월 28일 토요일';
+export const OT_DATE_TEXT_MAKERS = '8월 22일 토요일';
 
 export const getApplyInfo = (otDateText: string) => ({
   sections: [
